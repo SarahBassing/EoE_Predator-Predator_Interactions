ceb343f4007ba21aeec2d4967cebfcab7470ccf6
Sarah Bassing <sarah.bassing@gmail.com>
2022-09-22 16:59:00 -0700

    Why can't I save this in a nice function?!?!?

--- Scripts/Detection_data_cleaning.R
+++ Scripts/Detection_data_cleaning.R
@@ -42,44 +42,46 @@
     
     #'  Time triggered detections
     Tt <- dets_s20[dets_s20$TriggerMode == "T",]
-    wildlifeT <- Tt[Tt$Wildlife != FALSE,]
+    wildT <- Tt[Tt$Wildlife != FALSE,]
     humanT <- Tt[Tt$Human != FALSE,]
     vehicleT <- Tt[Tt$Vehicle != FALSE,]
     livestockT <- Tt[Tt$Livestock != FALSE,]
-    horseT <- Tt[Tt$Pet_pack_horse != FALSE,]
+    domesticT <- Tt[Tt$Pet_pack_horse != FALSE,]
     
     #'  List smaller data sets together
     dets_list <- list(Mt, wildM, humanM, vehicleM, livestockM, domesticM, 
-                      Tt, wildlifeT, humanT, vehicleT, livestockT, horseT)
+                      Tt, wildT, humanT, vehicleT, livestockT, domesticT)
+    names(dets_list) <- c("Mt", "wildM", "humanM", "vehicleM", "livestockM", "domesticM", 
+                          "Tt", "wildT", "humanT", "vehicleT", "livestockT", "domesticT")
     return(dets_list)
   }
   eoe20s_dets <- filter_dets(dets_s20)
   
-  #' #'  Motion triggered detections
-  #' eoe20s_M <- dets_s20[dets_s20$TriggerMode == "M",]
-  #' eoe20s_wildM <- eoe20s_M[eoe20s_M$Wildlife != FALSE,]
-  #' eoe20s_humanM <- eoe20s_M[eoe20s_M$Human != FALSE,]
-  #' eoe20s_vehicleM <- eoe20s_M[eoe20s_M$Vehicle != FALSE,]
-  #' eoe20s_livestockM <- eoe20s_M[eoe20s_M$Livestock != FALSE,]
-  #' eoe20s_domesticM <- eoe20s_M[eoe20s_M$Pet_pack_horse != FALSE,]
-  #' 
-  #' #'  Time triggered detections
-  #' eoe20s_T <- dets_s20[dets_s20$TriggerMode == "T",]
-  #' eoe20s_wildlifeT <- eoe20s_T[eoe20s_T$Wildlife != FALSE,]
-  #' eoe20s_humanT <- eoe20s_T[eoe20s_T$Human != FALSE,]
-  #' eoe20s_vehicleT <- eoe20s_T[eoe20s_T$Vehicle != FALSE,]
-  #' eoe20s_livestockT <- eoe20s_T[eoe20s_T$Livestock != FALSE,]
-  #' eoe20s_horseT <- eoe20s_T[eoe20s_T$Pet_pack_horse != FALSE,]
-  
   
   #'  Save smaller data sets as individual RData files
-  #'  This takes a LONG time to do
-  save_RData <- function(dat){
-    save(dat, file = paste("./Data/IDFG camera data/Split datasets/", dat, ".RData"))
+  save_Rdata <- function(dat, lname) {
+    # assign(paste(lname), dat)
+    save(dat, file = paste0("./Data/IDFG camera data/Split datasets/", lname, '.RData'))
   }
-  dat_list <- list(eoe20s_dets[[12]])
-  lapply(dat_list, save_RData)
-  # tst <- lapply(dat_list, save_RData)
+  dat_list <- list(eoe20s_dets[[11]], eoe20s_dets[[12]])
+  names(dat_list) <- c("tst1", "tst2")
+  lnames <- list("tst1", "tst2")
+  tst <- mapply(save_Rdata, dat_list, lnames)
+  
+  hkguyfy <- dat_list[[1]]
+  PLEASE <- dat_list
+  saveit <- function(...) {
+    x <- list(...)
+    save(list=names(x), file = paste0("./Data/IDFG camera data/Split datasets/workdammit.RData"))
+  }
+  saveit(hkguyfy)
+  saveit(dat_list)
+  load("./Data/IDFG camera data/Split datasets/workdammit.RData")
+  lapply(dat_list, saveit)
+  foo <- 1
+  saveit(bar=foo, file="hi.Rdata")
+  
+  
   
   library(parallel)
   #https://dept.stat.lsa.umich.edu/~jerrick/courses/stat701/notes/parallel.html
