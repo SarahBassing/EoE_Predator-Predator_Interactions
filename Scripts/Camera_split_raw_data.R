3e5587f1aca41df65e937457ada6ab51f5a263ec
Sarah Bassing <sarah.bassing@gmail.com>
2022-09-23 17:10:34 -0700

    Filter keepers

--- Scripts/Camera_split_raw_data.R
+++ Scripts/Camera_split_raw_data.R
@@ -6,8 +6,8 @@
   #'  ========================
   #'  Pull out, rename, and filter massive camera trap data sets into more 
   #'  manageable chunks and save for future analyses. Each data set is huge, 
-  #'  especially the wolf ones. Likely need to load and run script separately
-  #'  for each original file.
+  #'  especially the wolf ones. Likely need to load each data set and run script 
+  #'  separately because data sets are so large.
   #'  ========================
   
   #'  Clean workspace
@@ -17,6 +17,9 @@
   library(tidyverse)
   library(stringr)
   
+  #'  =======================================================
+  ####  Save deployment & detection data as separate files  ####
+  #'  =======================================================
   #'  EoE 2020 summer
   load("./Data/IDFG camera data/eoe2020s_all_final.RData")
   cams_s20_eoe <- dep_dat_done; save(cams_s20_eoe, file = "./Data/IDFG camera data/eoe2020s_cameras.RData")
@@ -48,7 +51,9 @@
   dets_s21_wolf <- pic_dat; save(dets_s21_wolf, file = "./Data/IDFG camera data/wolf2021s_detections.RData")
   
   
-  #'  Filter and save even further - need more manageable data sets to work with
+  #'  ======================================================
+  #####  Filter to more manageable data sets to work with  ####
+  #'  ======================================================
   #'  Detection data
   load("./Data/IDFG camera data/eoe2020s_detections.RData")
   load("./Data/IDFG camera data/eoe2020w_detections.RData")
@@ -91,6 +96,7 @@
   wolf20s_dets <- filter_dets(dets_s20_wolf, prefixname = "wolf20s_")
   wolf21s_dets <- filter_dets(dets_s21_wolf, prefixname = "wolf21s_")
   
+  
   #'  Save smaller data sets as individual RData files
   #'  https://stackoverflow.com/questions/30848091/saving-elements-of-a-list-as-data-frames-using-r
   save_Rdata <- function(dat) {
@@ -106,5 +112,48 @@
   save_Rdata(wolf19s_dets)
   save_Rdata(wolf20s_dets)
   save_Rdata(wolf21s_dets)
+  
+  
+  #'  =====================================================================
+  ####  Reduce datasets even further to just images flagged as "Keepers"  ####
+  #'  =====================================================================
+  #'  Motion triggered images
+  load("./Data/IDFG camera data/Split datasets/eoe20s_allM.RData")
+  load("./Data/IDFG camera data/Split datasets/eoe20w_allM.RData")
+  load("./Data/IDFG camera data/Split datasets/eoe21s_allM.RData")
+  
+  load("./Data/IDFG camera data/Split datasets/wolf19s_allM.RData")
+  load("./Data/IDFG camera data/Split datasets/wolf20s_allM.RData")
+  load("./Data/IDFG camera data/Split datasets/wolf21s_allM.RData")
+  
+  #'  Time triggered images
+  load("./Data/IDFG camera data/Split datasets/eoe20s_allT.RData")
+  load("./Data/IDFG camera data/Split datasets/eoe20w_allT.RData")
+  load("./Data/IDFG camera data/Split datasets/eoe21s_allT.RData")
+  
+  load("./Data/IDFG camera data/Split datasets/wolf19s_allT.RData")
+  load("./Data/IDFG camera data/Split datasets/wolf20s_allT.RData")
+  load("./Data/IDFG camera data/Split datasets/wolf21s_allT.RData")
+  
+  #'  Filter down to "Keepers"
+  keepers <- function(dets) {
+    keep_dets <- filter(dets, SetLocation == "KeepSet")
+    return(keep_dets)
+  }
+  eoe_motion <- list(eoe20s_allM, eoe20w_allM, eoe21s_allM)
+  eoe_motion_skinny <- lapply(eoe_motion, keepers)
+  save(eoe_motion_skinny, file = "./Data/IDFG camera data/Split datasets/eoe_motion_skinny.RData")
+  
+  eoe_time <- list(eoe20s_allT, eoe20w_allT, eoe21s_allT)
+  eoe_time_skinny <- lapply(eoe_time, keepers)
+  save(eoe_time_skinny, file = "./Data/IDFG camera data/Split datasets/eoe_time_skinny.RData")
+  
+  wolf_motion <- list(wolf19s_allM, wolf20s_allM, wolf21s_allM)
+  wolf_motion_skinny <- lapply(wolf_motion, keepers)
+  save(wolf_motion_skinny, file = "./Data/IDFG camera data/Split datasets/wolf_motion_skinny.RData")
+  
+  wolf_time <- list(wolf19s_allT, wolf20s_allT, wolf21s_allT)
+  wolf_time_skinny <- lapply(wolf_time, keepers)
+  save(wolf_time_skinny, file = "./Data/IDFG camera data/Split datasets/wolf_time_skinny.RData")
   
   
