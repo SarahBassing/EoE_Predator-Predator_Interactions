17cb2f4a6743ad19e7fd0b2d5fcd1e4b369c641b
Sarah Bassing <sarah.bassing@gmail.com>
2022-11-15 12:29:36 -0800

    add NewLocationID & camera setup info to image data

--- Scripts/Detection_data_cleaning.R
+++ Scripts/Detection_data_cleaning.R
@@ -15,10 +15,10 @@
   library(tidyverse)
   
   #'  Camera locations
-  cams_eoe <- read.csv("./Data/IDFG camera data/cams_eoe_skinny.csv") %>%
-    dplyr::select(-X)
   cams_eoe_long <- read.csv("./Data/IDFG camera data/cams_eoe_long.csv") %>%
     dplyr::select(-X)
+  # cams_eoe <- read.csv("./Data/IDFG camera data/cams_eoe_skinny.csv") %>%
+  #   dplyr::select(-X)
   # cams_wolf <- read.csv("./Data/IDFG camera data/cams_wolf_skinny.csv") %>%
   #   dplyr::select(-X)
   
@@ -47,7 +47,32 @@
   load("./Data/IDFG camera data/Split datasets/wolf_motion_skinny.RData")
   load("./Data/IDFG camera data/Split datasets/wolf_time_skinny.RData")
   
+  #'  Add camera setup info (P/U/O/A) and consistent naming structure to match 
+  #'  camera location data
+  add_deploy_info <- function(dets, season, pred) { 
+    sub_cams <- cams_eoe_long %>%
+      filter(Season == season) %>%
+      filter(Setup == pred)
+    pcams <- dets$CamID %in% sub_cams$CamID
+    dets$pcams <- pcams
+    dets <- dets %>%
+      mutate(Setup = ifelse(pcams == "TRUE", "P", "U"),
+             NewLocationID = paste0("GMU", Gmu, "_", Setup, "_", LocationID)) %>%
+      dplyr::select(-pcams)
+    return(dets)
+  }
+  #'  EoE cameras
+  eoe_seasons <- list("Smr20", "Wtr20", "Smr21")
+  eoe_motion_list <- mapply(add_deploy_info, season = eoe_seasons, dets = eoe_motion_skinny, pred = "predator", SIMPLIFY = FALSE) 
+  eoe_time_list <- mapply(add_deploy_info, season = eoe_seasons, dets = eoe_time_skinny, pred = "predator", SIMPLIFY = FALSE)  
+  #'  Wolf cameras
+  wolf_seasons <- list("Smr20", "Wtr20", "Smr21")
+  wolf_motion_list <- mapply(add_deploy_info, season = wolf_seasons, dets = wolf_motion_skinny, pred = "predator", SIMPLIFY = FALSE)  
+  wolf_time_list <- mapply(add_deploy_info, season = wolf_seasons, dets = wolf_time_skinny, pred = "predator", SIMPLIFY = FALSE)  
   
-  eoe20s_noon <- eoe_time_skinny[[1]]
+  
+  
+    
+  eoe20s_noon <- eoe_time_list[[1]]
   
   
