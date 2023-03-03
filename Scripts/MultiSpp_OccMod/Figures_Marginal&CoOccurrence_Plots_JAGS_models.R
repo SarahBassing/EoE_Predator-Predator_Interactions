a134e5cd9520f6d417f1d3382e91bfe086676d5f
Sarah Bassing <sarah.bassing@gmail.com>
2023-03-03 13:25:15 -0800

    Create functions to predict occupancy probabilites and plot

--- Scripts/MultiSpp_OccMod/Figures_Marginal&CoOccurrence_Plots_JAGS_models.R
+++ Scripts/MultiSpp_OccMod/Figures_Marginal&CoOccurrence_Plots_JAGS_models.R
@@ -10,160 +10,201 @@
   
   #'  Load libraries
   library(ggplot2)
+  library(tidyverse)
   
-  #'  Load saved JAGS output and covariate data
-  load("./Outputs/MultiSpp_OccMod_Outputs/JAGS_output/coybob_psi(setup_preydiversity)_p(setup_effort).RData") #coybob_psi(habitat)_p(effort)
+  #'  Load covariate data
   load("./Data/Covariates_extracted/Covariate_skinny_EoE20s21s.RData")
   
-  out1 <- coy.bob.preydiversity
-  
-  #'  Define number of states
-  ncat <- 4
-  #'  Number of draws in the MCMC chain
-  ndraws <- out1$mcmc.info$n.samples
-  #'  Number of expected observations to predict
-  npoints <- 1000
-  
-  #'  Grab posterior means of parameters
-  # (tmp <- out1$mean[1:5])
-  print(out1$summary[1:44,])
-  
-  #'  Create prediction covariates for occupancy as a function of focal covariate
-  (r <- range(stations_skinny_eoe20s21s$Nwtd))
-  (mean.cov <- mean(stations_skinny_eoe20s21s$Nwtd))
-  (sd.cov<- sd(stations_skinny_eoe20s21s$Nwtd))
-  #'  Create new data based on range of sampled covariate data
-  cov.pred.orig <- seq(r[1], r[2], length.out = npoints) 
-  #'  Scale new data to be consistent with model input
-  cov.pred <- (cov.pred.orig - mean.cov) / sd.cov
-  
-  #'  Create model matrices for the prediction covariates in order of model coefficients
-  #'  Vary one covariate while holding others constant
-  head(psi_cov <- cbind(1, 1, cov.pred, 0, 0, 0, 0, 0, 0)) #' intercept, setup, elevation, forest, elk, moose, md, wtd, lagomorph
-  
-  #'  Assemble linear predictors across all iterations of the MCMC chains
-  psiSpp1 <- out1$sims.list$betaSpp1 %*% t(psi_cov)
-  psiSpp2 <- out1$sims.list$betaSpp2 %*% t(psi_cov)
-  psiSpp12 <- psiSpp1 + psiSpp2 + out1$sims.list$betaSpp12[,1:9] %*% t(psi_cov) #drop the indixing once I rerun without the unused priors
-  
-  ####  Assemble the state probability array  ####
-  #'  Compute latent state vector (lsv)
-  lsv <- array(NA, dim = c(ndraws, npoints, ncat))
-  lsv[,,1] <- 1
-  lsv[,,2] <- exp(psiSpp1)
-  lsv[,,3] <- exp(psiSpp2)
-  lsv[,,4] <- exp(psiSpp12)
-
-  #'  Probability of each state as MCMC chains (lsp)
-  #'  denominator  = sum of lsv for each draw x point
-  denom <- apply(lsv, 1:2, sum)
-  lspMC <- sweep(lsv, 1:2, denom, "/")
-  dimnames(lspMC)[[3]] <- c("U", "Spp1", "Spp2", "Both")
-  str(lspMC)
-  
-  #'  Fill in summary values for 'lsp'
-  lsp <- array(NA, dim = c(npoints, ncat, 4))
-  dimnames(lsp) <- list(NULL, c("U", "Spp1", "Spp2", "Both"),
-                        c("mean", "SD", "lower", "upper"))
-  lsp[,,1] <- apply(lspMC, 2:3, mean)
-  lsp[,,2] <- apply(lspMC, 2:3, sd)
-  lsp[,,3] <- apply(lspMC, 2:3, quantile, probs = 0.025)
-  lsp[,,4] <- apply(lspMC, 2:3, quantile, probs = 0.975)
-  
-  #'  Plot the latent state probabilities as a function of covariate
-  matplot(cov.pred.orig, lsp[,,"mean"], type = "l", lty = 1, lwd = 5, col = 1:4,
-          frame = FALSE, xlab = "Covariate", ylab = "Probability of State",
-          ylim = c(0, max(lsp[,,-2])), las = 1)
-  #'  Add CRIs
-  for(i in 1:4){
-    polygon(c(cov.pred.orig, rev(cov.pred.orig)), c(lsp[,i,"lower"], rev(lsp[,i,"upper"])),
-            border = NA, col = adjustcolor(i, alpha = 0.2))
+  ####  Predict marginal & conditional occupancy  ####
+  #'  --------------------------------------------
+  #'  MASSIVE function the predict marginal and conditional occupancy for each species
+  predict_occupancy <- function(mod, ncat, npoints, focal_cov, psi_cov, cov_index) {
+    #'  Rename model and review
+    out1 <- mod
+    print(out1$summary[1:50,])
+    #'  Number of states
+    ncat <- ncat
+    #'  Number of draws in the MCMC chain
+    ndraws <- out1$mcmc.info$n.samples
+    #'  Number of observations to predict
+    npoints <- npoints
+    
+    #'  Create range of covariate values to predict across
+    print(r <- range(focal_cov))
+    print(mean.cov <- mean(focal_cov))
+    print(sd.cov <- sd(focal_cov))
+    cov.pred.orig <- seq(r[1], r[2], length.out = npoints)
+    #'  Scale new data to be consistent with data used in model
+    cov.pred <- (cov.pred.orig - mean.cov) / sd.cov
+    
+    #'  Create model matrices for prediction covariates
+    #'  Fill matrix with categorical or mean value of each covariate
+    psi_cov <- psi_cov
+    psi_covs <- matrix(psi_cov, nrow = npoints, ncol = length(psi_cov), byrow = TRUE)
+    #'  Replace focal covariate column with new scaled data
+    #'  New data must be in correct column based on order of covariates in original model
+    psi_covs[,cov_index] <- cov.pred
+    print(head(psi_covs))
+    
+    #'  Assemble linear predictors across all iterations of the MCMC chains
+    psiSpp1 <- out1$sims.list$betaSpp1 %*% t(psi_covs)
+    psiSpp2 <- out1$sims.list$betaSpp2 %*% t(psi_covs)
+    psiSpp12 <- psiSpp1 + psiSpp2 + out1$sims.list$betaSpp12 %*% t(psi_covs) #betaSpp12[,1:9] for current saved coy.bob.preydiversity model
+    
+    #'  Compute latent state vector (lsv) based on new data and means of other covariates
+    lsv <- array(NA, dim = c(ndraws, npoints, ncat))
+    lsv[,,1] <- 1
+    lsv[,,2] <- exp(psiSpp1)
+    lsv[,,3] <- exp(psiSpp2)
+    lsv[,,4] <- exp(psiSpp12)
+    
+    #'  Compute latent state probability of each state from MCMC chains (lsp)
+    #'  denominator  = sum of lsv for each draw x point
+    denom <- apply(lsv, 1:2, sum)
+    lspMC <- sweep(lsv, 1:2, denom, "/")
+    dimnames(lspMC)[[3]] <- c("U", "Spp1", "Spp2", "Both")
+    str(lspMC)
+    
+    #'  Fill in summary values for 'lsp'
+    lsp <- array(NA, dim = c(npoints, ncat, 4))
+    dimnames(lsp) <- list(NULL, c("U", "Spp1", "Spp2", "Both"),
+                          c("mean", "SD", "lower", "upper"))
+    lsp[,,1] <- apply(lspMC, 2:3, mean)
+    lsp[,,2] <- apply(lspMC, 2:3, sd)
+    lsp[,,3] <- apply(lspMC, 2:3, quantile, probs = 0.025)
+    lsp[,,4] <- apply(lspMC, 2:3, quantile, probs = 0.975)
+    
+    #'  Compute and plot marginal probabilities
+    #'  --------------------------------------- 
+    #'  Occupancy probability for each species, regardless of presence or
+    #'  absence of other species
+    #'  Get MCMC chains
+    marginalMC <- array(NA, dim = c(ndraws, npoints, 2))
+    dimnames(marginalMC)[[3]] <- c("Spp1", "Spp2")
+    marginalMC[,,"Spp1"] <- apply(lspMC[,,c("Spp1", "Both")], 1:2, sum)
+    marginalMC[,,"Spp2"] <- apply(lspMC[,,c("Spp2", "Both")], 1:2, sum)
+    
+    #'  Fill in summary values for marginal probabilities
+    marginal <- array(NA, dim = c(npoints, 2, 4))
+    dimnames(marginal) <- list(NULL, c("Spp1", "Spp2"),
+                               c("mean", "sd", "lower", "upper"))
+    marginal[,,1] <- apply(marginalMC, 2:3, mean)
+    marginal[,,2] <- apply(marginalMC, 2:3, sd)
+    marginal[,,3] <- apply(marginalMC, 2:3, quantile, probs = 0.025)
+    marginal[,,4] <- apply(marginalMC, 2:3, quantile, probs = 0.975)
+    
+    #'  Compute conditional occupancy for all species
+    #'  --------------------------------------------- 
+    #'  Occupancy probability for each species, conditional on the presence or
+    #'  absence of the other species
+    #'  Get MCMC chains
+    conditionalMC <- array(NA, dim = c(ndraws, npoints, 4))
+    dimnames(conditionalMC)[[3]] <- c("Spp1.alone", "Spp1.given.Spp2",
+                                      "Spp2.alone", "Spp2.given.Spp1")
+    
+    conditionalMC[,,"Spp1.alone"] <- lspMC[,,"Spp1"]/apply(lspMC[,,c("U", "Spp1")], 1:2, sum)
+    conditionalMC[,,"Spp1.given.Spp2"] <- lspMC[,,"Both"]/marginalMC[,,"Spp2"]
+    conditionalMC[,,"Spp2.alone"] <- lspMC[,,"Spp2"]/apply(lspMC[,,c("U", "Spp2")], 1:2, sum)
+    conditionalMC[,,"Spp2.given.Spp1"] <- lspMC[,,"Both"]/marginalMC[,,"Spp1"]
+    
+    #'  Fill in summary values
+    conditional <- array(NA, dim = c(npoints, 4, 4))
+    dimnames(conditional) <- list(NULL, c("Spp1.alone", "Spp1.given.Spp2",
+                                          "Spp2.alone", "Spp2.given.Spp1"),
+                                  c("mean", "sd", "lower", "upper"))
+    conditional[,,1] <- apply(conditionalMC, 2:3, mean)
+    conditional[,,2] <- apply(conditionalMC, 2:3, sd)
+    conditional[,,3] <- apply(conditionalMC, 2:3, quantile, probs = 0.025)
+    conditional[,,4] <- apply(conditionalMC, 2:3, quantile, probs = 0.975)
+    
+    #'  List predicted probabilities and new covariate data to plot
+    predicted_probabilities <- list(marginal, conditional, cov.pred.orig, cov.pred)
+    
+    return(predicted_probabilities)
   }
-  legend("topright", lwd = 3, lty = 1, col = 1:4, colnames(lsp), bty = "n", cex = 1.2, ncol = 2)
   
-  #'  Or plot them separately
-  op <- par(mfrow=c(2,2))
-  for(i in 1:4) {
-    plot(cov.pred.orig, lsp[,i,'mean'], type = 'n',
-         frame = FALSE, xlab = 'Covariate', ylab = 'Probability of state',
-         ylim = c(0, max(lsp[,,-2])), las = 1)
-    polygon(c(cov.pred.orig, rev(cov.pred.orig)), c(lsp[,i,'lower'], rev(lsp[,i,'upper'])),
-            border=NA, col=adjustcolor(i, alpha=0.2))
-    lines(cov.pred.orig, lsp[,i,'mean'], lwd = 2, col = i)
-    legend('topright', legend=colnames(lsp)[i], lwd = 2, col = i, bty = 'n')
+  #####  Coyote-Bobcat co-occurrence  ####
+  load("./Outputs/MultiSpp_OccMod_Outputs/JAGS_output/coybob_psix(setup_habitat_rx)_px(setup_effort).RData")
+  coy.bob.elev.pred <- predict_occupancy(mod = coy.bob.hab, ncat = 4, npoints = 1000,
+                                         focal_cov = stations_skinny_eoe20s21s$Elev,
+                                         psi_cov = c(1, 1, 0, 0), cov_index = 3)
+  coy.bob.forest.pred <- predict_occupancy(mod = coy.bob.hab, ncat = 4, npoints = 1000,
+                                           focal_cov = stations_skinny_eoe20s21s$PercForest,
+                                           psi_cov = c(1, 1, 0, 0), cov_index = 4)
+  
+  
+  
+  ####  Plot co-occurrence probabilities  ####
+  #'  ------------------------------------
+  #'  Function to plot marginal probabilities
+  marginal_prob_plots <- function(focal_cov, npoints, marg, covariate_name, spp1, spp2, plotit = T) {
+    
+    #'  Create range of covariate values to plot
+    r <- range(focal_cov)
+    mean.cov <- mean(focal_cov)
+    sd.cov <- sd(focal_cov)
+    cov.pred.orig <- seq(r[1], r[2], length.out = npoints)
+    #'  Scale new data to be consistent with data used in model
+    cov.pred <- (cov.pred.orig - mean.cov) / sd.cov
+    
+    #'  Plot marginal probability for each species
+    matplot(cov.pred.orig, marg[,,"mean"],
+            type = "l", lty = 1:2, lwd = 3, col = 1:2, frame = FALSE, xlab = covariate_name,
+            ylab = "Marginal occupancy probability", ylim = c(0, max(marg[,,"upper"])),
+            main = paste("Marginal Occupancy for", spp1, "and", spp2))
+    #'  Add CRIs
+    for(i in 1:2) {
+      polygon(c(cov.pred.orig, rev(cov.pred.orig)), c(marg[,i,"lower"], rev(marg[,i,"upper"])),
+              border = NA, col = adjustcolor(i, alpha = 0.2))
+    }
+    legend("bottom", c(spp1, spp2), lwd = 3, lty = 1:2, col = 1:2, horiz = TRUE, bty = "n")
   }
-  par(op)
+  marginal_prob_plots(focal_cov = stations_skinny_eoe20s21s$Elev, npoints = 1000,
+                      marg = coy.bob.elev.pred[[1]],
+                      covariate_name = "Elevation (m)", spp1 = "Coyote", spp2 = "Bobcat")
   
-  ####  Compute and plot marginal probabilities  ####
-  #'  Get MCMC chains
-  marginalMC <- array(NA, dim = c(ndraws, npoints, 2))
-  dimnames(marginalMC)[[3]] <- c("Spp1", "Spp2")
-  marginalMC[,,"Spp1"] <- apply(lspMC[,,c("Spp1", "Both")], 1:2, sum)
-  marginalMC[,,"Spp2"] <- apply(lspMC[,,c("Spp2", "Both")], 1:2, sum)
   
-  #'  Fill in summary values for marginal probabilities
-  marginal <- array(NA, dim = c(npoints, 2, 4))
-  dimnames(marginal) <- list(NULL, c("Spp1", "Spp2"),
-                             c("mean", "sd", "lower", "upper"))
-  marginal[,,1] <- apply(marginalMC, 2:3, mean)
-  marginal[,,2] <- apply(marginalMC, 2:3, sd)
-  marginal[,,3] <- apply(marginalMC, 2:3, quantile, probs = 0.025)
-  marginal[,,4] <- apply(marginalMC, 2:3, quantile, probs = 0.975)
   
-  #'  Plot marginal occupancy for each species
-  matplot(cov.pred.orig, marginal[,,"mean"],
-          type = "l", lty = 1:2, lwd = 3, col = 1:2, frame = FALSE, xlab = "Covariate",
-          ylab = "Marginal occupancy probability", ylim = c(0, max(marginal[,,"upper"])))
-  #'  Add CRIs
-  for(i in 1:2) {
-    polygon(c(cov.pred.orig, rev(cov.pred.orig)), c(marginal[,i,"lower"], rev(marginal[,i,"upper"])),
-            border = NA, col = adjustcolor(i, alpha = 0.2))
+  
+  #'  Function to plot conditional probabilities
+  conditional_prob_plot <- function(focal_cov, npoints, cond, covariate_name, spp1, spp2, plotit = T) {
+    
+    #'  Create range of covariate values to plot
+    r <- range(focal_cov)
+    mean.cov <- mean(focal_cov)
+    sd.cov <- sd(focal_cov)
+    cov.pred.orig <- seq(r[1], r[2], length.out = npoints)
+    #'  Scale new data to be consistent with data used in model
+    cov.pred <- (cov.pred.orig - mean.cov) / sd.cov
+    
+    #'  Plot conditional probabilities of occupancy
+    op <- par(mfrow = c(1,2))
+    matplot(cov.pred.orig, cond[,1:2,"mean"],
+            type = "l", lty = 1:2, lwd = 3, col = 1:2, frame = FALSE,
+            xlab = covariate_name, ylab = "Occupancy probability",
+            ylim = c(0, 1), main = spp1)
+    for(i in 1:2){
+      polygon(c(cov.pred.orig, rev(cov.pred.orig)), c(cond[,i,"lower"], rev(cond[,i,"upper"])),
+              border = NA, col = adjustcolor(i, alpha = 0.2))
+    }
+    legend("bottomright", c("alone", paste("given", spp2)), lwd = 3, lty = 1:3, col = 1:3, bty = 'n', cex = 1.5)
+    
+    matplot(cov.pred.orig, cond[,3:4,"mean"],
+            type = "l", lty = 1:2, lwd = 3, col = 1:2, frame = FALSE,
+            xlab = covariate_name, ylab = "Occupancy probability",
+            ylim = c(0, 1), main = spp2)
+    for(i in 1:2){
+      polygon(c(cov.pred.orig, rev(cov.pred.orig)), c(cond[,i+2,"lower"], rev(cond[,i+2,"upper"])),
+              border = NA, col = adjustcolor(i, alpha = 0.2))
+    }
+    legend("bottomright", c("alone", paste("given", spp1)), lwd = 3, lty = 1:3, col = 1:3, bty = 'n', cex = 1.5)
+    par(op)
   }
-  legend("bottom", c("Spp1", "Spp2"), lwd = 3, lty = 1:2, col = 1:2, horiz = TRUE, bty = "n")
+  conditional_prob_plot(focal_cov = stations_skinny_eoe20s21s$Elev, npoints = 1000,
+                        covariate_name = "Percent Forest Cover", cond = coy.bob.elev.pred[[2]],
+                        spp1 = "coyote", spp2 = "bobcat")
   
-  ####  Compute conditional occupancy for all species  ####
-  #'  Get MCMC chains
-  conditionalMC <- array(NA, dim = c(ndraws, npoints, 4))
-  dimnames(conditionalMC)[[3]] <- c("Spp1.alone", "Spp1.given.Spp2",
-                                    "Spp2.alone", "Spp2.given.Spp1")
   
-  conditionalMC[,,"Spp1.alone"] <- lspMC[,,"Spp1"]/apply(lspMC[,,c("U", "Spp1")], 1:2, sum)
-  conditionalMC[,,"Spp1.given.Spp2"] <- lspMC[,,"Both"]/marginalMC[,,"Spp2"]
-  conditionalMC[,,"Spp2.alone"] <- lspMC[,,"Spp2"]/apply(lspMC[,,c("U", "Spp2")], 1:2, sum)
-  conditionalMC[,,"Spp2.given.Spp1"] <- lspMC[,,"Both"]/marginalMC[,,"Spp1"]
-  
-  #'  Fill in summary values
-  conditional <- array(NA, dim = c(npoints, 4, 4))
-  dimnames(conditional) <- list(NULL, c("Spp1.alone", "Spp1.given.Spp2",
-                                        "Spp2.alone", "Spp2.given.Spp1"),
-                                c("mean", "sd", "lower", "upper"))
-  conditional[,,1] <- apply(conditionalMC, 2:3, mean)
-  conditional[,,2] <- apply(conditionalMC, 2:3, sd)
-  conditional[,,3] <- apply(conditionalMC, 2:3, quantile, probs = 0.025)
-  conditional[,,4] <- apply(conditionalMC, 2:3, quantile, probs = 0.975)
-  
-  #'  Plot conditional probabilities of occupancy
-  op <- par(mfrow = c(1,2))
-  matplot(cov.pred.orig, conditional[,1:2,"mean"],
-          type = "l", lty = 1:2, lwd = 3, col = 1:2, frame = FALSE,
-          xlab = "Covariate", ylab = "Occupancy probability",
-          ylim = c(0, 1), main = "Spp1")
-  for(i in 1:2){
-    polygon(c(cov.pred.orig, rev(cov.pred.orig)), c(conditional[,i,"lower"], rev(conditional[,i,"upper"])),
-            border = NA, col = adjustcolor(i, alpha = 0.2))
-  }
-  legend("bottomright", c("alone", "given Spp2"), lwd = 3, lty = 1:3, col = 1:3, bty = 'n', cex = 1.5)
-  
-  matplot(cov.pred.orig, conditional[,3:4,"mean"],
-          type = "l", lty = 1:2, lwd = 3, col = 1:2, frame = FALSE,
-          xlab = "Covariate", ylab = "Occupancy probability",
-          ylim = c(0, 1), main = "Spp2")
-  for(i in 1:2){
-    polygon(c(cov.pred.orig, rev(cov.pred.orig)), c(conditional[,i+2,"lower"], rev(conditional[,i+2,"upper"])),
-            border = NA, col = adjustcolor(i, alpha = 0.2))
-  }
-  legend("topright", c("alone", "given Spp1"), lwd = 3, lty = 1:3, col = 1:3, bty = 'n', cex = 1.5)
-  par(op)
   
   
   
