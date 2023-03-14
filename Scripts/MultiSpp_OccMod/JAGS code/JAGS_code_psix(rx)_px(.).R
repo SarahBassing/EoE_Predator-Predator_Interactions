474567de9b5a99220846970dd7506ae0a958ca6f
Sarah Bassing <sarah.bassing@gmail.com>
2023-03-14 09:07:59 -0700

    fix text file name

--- Scripts/MultiSpp_OccMod/JAGS code/JAGS_code_psix(rx)_px(.).R
+++ Scripts/MultiSpp_OccMod/JAGS code/JAGS_code_psix(rx)_px(.).R
@@ -5,7 +5,7 @@
   #'  February 2023
   #'  ------------------------------------
 
-cat(file = './Outputs/MultiSpp_OccMod_Outputs/JAGS_output/JAGS_code_psix(.rx)_px(.).txt', "
+cat(file = './Outputs/MultiSpp_OccMod_Outputs/JAGS_output/JAGS_code_psix(rx)_px(.).txt', "
     model{
           
       #### Define Priors  ####
