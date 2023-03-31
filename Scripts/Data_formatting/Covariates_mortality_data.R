e17893fb9e0a985a27ea813bc60b591e5ced1a39
Sarah Bassing <sarah.bassing@gmail.com>
2023-03-31 13:26:45 -0700

    reformat using updated bgmr data

--- Scripts/Data_formatting/Covariates_mortality_data.R
+++ Scripts/Data_formatting/Covariates_mortality_data.R
@@ -16,6 +16,8 @@
   #'  Load libraries
   library(sf) 
   library(units)
+  library(lubridate)
+  library(stringr)
   library(tidyverse)
   
   #'  Load camera locations
@@ -24,23 +26,69 @@
   load("./Data/IDFG camera data/Problem cams/eoe21s_problem_cams.RData")
   
   #'  Load mortality data
-  mort_bear <- read.csv("./Data/IDFG BGMR data/blackbear_GMU1_6_10A.csv")
+  mort_bear <- read.csv("./Data/IDFG BGMR data/Black Bear_BGMR_Data (2).csv") %>% #blackbear_GMU1_6_10A
+    transmute(Species = Species,
+              Kill.Date = as_date(Kill.Date, format = "%Y-%m-%d"),
+              Game.Management.Unit = Game.Management.Unit,
+              Method = Mortality.Agent, 
+              Season = Harvest.Year,
+              LICYEAR = NA) %>% 
+    mutate(Method = str_to_title(Method)) %>%
+    filter(Game.Management.Unit == "1" | Game.Management.Unit == "6" | Game.Management.Unit == "10A") %>%
+    filter(Season >= "2017" & Season < "2022")
   
-  mort_bob <- read.csv("./Data/IDFG BGMR data/bobcat_GMU1_6_10A.csv") %>%
+  mort_bob <- read.csv("./Data/IDFG BGMR data/Bobcats All Harvest.csv") %>% #bobcat_GMU1_6_10A
     transmute(Species = "Bobcat",
-              Mortality.Agent = NA,
               Kill.Date = DATE_TAKEN,
               Game.Management.Unit = UNIT,
               Method = METHOD_TAKE_DESC, 
-              Comments = COMMENT) %>%
-    mutate(Mortality.Agent = ifelse(Method == "TRAPPING", "Trapping", Method),
-           Mortality.Agent = ifelse(Mortality.Agent == "FOUND" | Mortality.Agent == "", "Other", Mortality.Agent),
-           Mortality.Agent = ifelse(Mortality.Agent != "Trapping" & Mortality.Agent != "Other", "Hunting", Mortality.Agent))
-  mort_lion <- read.csv("./Data/IDFG BGMR data/mountainlion_GMU1_6_10A.csv")
-  mort_wolf <- read.csv("./Data/IDFG BGMR data/wolf_GMU1_6_10A.csv")
+              Season = SEASON,
+              LICYEAR = TLIC_Year) %>%
+    filter(Game.Management.Unit == "1" | Game.Management.Unit == "6" | Game.Management.Unit == "10A") %>%
+    filter(Season >= "2017" & Season < "2022") %>%
+    mutate(Kill.Date = gsub(" ", "", Kill.Date),
+           Kill.Date = as_date(Kill.Date, format = "%m/%d/%Y"),
+           Method = str_to_title(Method))
+  
+  mort_lion <- read.csv("./Data/IDFG BGMR data/Mountain Lion_BGMR_Data (2).csv") %>% #mountainlion_GMU1_6_10A
+    transmute(Species = Species,
+              Kill.Date = Kill.Date,
+              Game.Management.Unit = Game.Management.Unit,
+              Method = Mortality.Agent, 
+              Season = Harvest.Year,
+              LICYEAR = NA) %>%
+    filter(Game.Management.Unit == "1" | Game.Management.Unit == "6" | Game.Management.Unit == "10A") %>%
+    filter(Season >= "2017" & Season < "2022") %>%
+    mutate(Kill.Date = gsub(" ", "", Kill.Date),
+           Kill.Date = as_date(Kill.Date, format = "%m/%d/%Y"),
+           Method = str_to_title(Method))
+  
+  mort_coy <- read.csv("./Data/IDFG BGMR data/Coyotes Harvested by County.csv") %>%
+    transmute(Species = SPECODE,
+              Kill.Date = NA,
+              Game.Mangament.Unit = NA,
+              method = NA,
+              Season = NA,
+              LICYEAR = LICYEAR,
+              County = COUNYTRA) %>%
+    filter(LICYEAR >= "2017" & LICYEAR < "2022")
+  
+  mort_wolf <- read.csv("./Data/IDFG BGMR data/wolf_GMU1_6_10A.csv") %>%
+    transmute(Species = Species,
+              Kill.Date = Kill.Date,
+              Game.Management.Unit = Game.Management.Unit,
+              Method = Mortality.Agent, 
+              Season = NA,
+              LICYEAR = NA) %>%
+    filter(Game.Management.Unit == "1" | Game.Management.Unit == "6" | Game.Management.Unit == "10A") %>%
+    mutate(Kill.Date = gsub(" ", "", Kill.Date),
+           Kill.Date = as_date(Kill.Date, format = "%m/%d/%Y"),
+           Season = year(Kill.Date),
+           Method = str_to_title(Method)) %>%
+    filter(Season >= "2017" & Season < "2022")
   
   #'  List all mortality data together
-  mort_predators <- list(mort_bear, mort_bob, mort_lion, mort_wolf)
+  mort_predators <- list(mort_bear, mort_bob, mort_lion, mort_wolf) #mort_coy, 
   
   #'  -----------------------------------------------------
   ####  Filter mortality data to relevant sources & times  ####
@@ -49,12 +97,12 @@
   #'  mortality owing to incomplete reporting.
   legal_mortality <- function(mort) {
     mort <- mort %>%
-      filter(Mortality.Agent != "Road Kill") %>%
-      filter(Mortality.Agent != "Unknown") %>%
-      filter(Mortality.Agent != "Illegal Kill") %>%
-      filter(Mortality.Agent != "Other") %>%
-      filter(Mortality.Agent != "Natural Mortality") 
-    print(unique(mort$Mortality.Agent))
+      filter(Method != "Road Kill") %>%
+      filter(Method != "Unknown") %>%
+      filter(Method != "Illegal Kill") %>%
+      filter(Method != "Other") %>%
+      filter(Method != "Natural Mortality") 
+    print(unique(mort$Method))
     return(mort)
   }
   mort_predators_legal <- lapply(mort_predators, legal_mortality)
@@ -64,15 +112,15 @@
   #'  data; winter 2020 - fall 2020 for Wrt20; fall 2020 - spring 2021 for Smr21 data)
   annual_mortality <- function(mort, start_date, end_date) {
     mort <- mort %>%
-      mutate(Kill.Date = gsub(" ", "", Kill.Date), # get rid of white spaces in date
-             Kill.Date = as.Date(Kill.Date, format = "%m/%d/%Y")) %>%
+      # mutate(Kill.Date = gsub(" ", "", Kill.Date), # get rid of white spaces in date
+      #        Kill.Date = as.Date(Kill.Date, format = "%m/%d/%Y")) %>%
       filter(Kill.Date >= start_date) %>%
       filter(Kill.Date <= end_date)
     return(mort)
   }
-  mort_pre_Smr20 <- lapply(mort_predators_legal, annual_mortality, start_date = "2019-09-01", end_date = "2020-06-30")
+  mort_pre_Smr20 <- lapply(mort_predators_legal, annual_mortality, start_date = "2019-08-01", end_date = "2020-03-31")
   mort_pre_Wtr20 <- lapply(mort_predators_legal, annual_mortality, start_date = "2020-02-01", end_date = "2020-11-30")
-  mort_pre_Smr21 <- lapply(mort_predators_legal, annual_mortality, start_date = "2020-09-01", end_date = "2021-06-30")
+  mort_pre_Smr21 <- lapply(mort_predators_legal, annual_mortality, start_date = "2020-08-01", end_date = "2021-03-31")
   
   
   #'  ---------------------
