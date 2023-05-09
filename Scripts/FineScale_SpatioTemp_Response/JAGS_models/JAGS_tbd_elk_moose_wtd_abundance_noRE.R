244e5511faf16b2ef3b6868a4f809890d2151118
Sarah Bassing <sarah.bassing@gmail.com>
2023-05-09 12:37:58 -0700

    JAGS code for tbd analyses, different combos of covariates, with & without random effect for site

---
+++ Scripts/FineScale_SpatioTemp_Response/JAGS_models/JAGS_tbd_elk_moose_wtd_abundance_noRE.R
@@ -0,0 +1,56 @@
+  #'  ----------------------------------------
+  #'  Latency model - prey RIA (elk, moose, wtd) effect, no random effect
+  #'  ID CRU - Predator Interactions
+  #'  Sarah Bassing
+  #'  May 2023
+  #'  ----------------------------------------
+  #'  Model sourced to Model_TBD_with_GLMMs.R
+  #'  
+  #'  Estimate mean number of minutes elapsed between sequential detections of
+  #'  different predators and test whether relative abundance of elk/moose/wtd influences TBD. 
+  #'  -----------------------------------------
+  
+  cat(file = "./Outputs/Time_btwn_Detections/tbd_elk_moose_wtd_abundance_noRE.txt", "
+      model{
+      
+      #'  Priors
+      #'  ------
+      alpha0 ~ dnorm(0, 0.01)
+      
+      for(pp in 1:npp) {
+        beta.prey[pp] ~ dnorm(0, 0.01)
+      }
+    
+      
+      #'  Define likelihood
+      #'  -----------------
+      for(i in 1:ntbd){
+        y[i] ~ dexp(tbd_lambda[i])
+        
+        tbd_lambda[i] <- 1/tbd_mu[i]
+      
+        log(tbd_mu[i]) <- alpha0 + beta.prey[1]*covs[i,5] + beta.prey[2]*covs[i,6] + beta.prey[3]*covs[i,8] 
+      }
+      
+      
+      #'  Derived parameters
+      #'  ------------------
+      #'  Mean TBD at average prey RAI
+      mu.tbd <- exp(alpha0 + beta.prey[1]*0 + beta.prey[2]*0 + beta.prey[3]*0)
+      
+      #'  Mean TBD across range of elk relative abundance values
+      for(i in 1:100){
+        spp.tbd.elk[i] <- exp(alpha0 + beta.prey[1]*newcovs[i,1] + beta.prey[2]*0 + beta.prey[3]*0)
+      }
+      
+      #'  Mean TBD across range of moose relative abundance values 
+      for(i in 1:100){
+        spp.tbd.moose[i] <- exp(alpha0 + beta.prey[1]*0 + beta.prey[2]*newcovs[i,2] + beta.prey[3]*0)
+      }
+      
+      #'  Mean TBD across range of wtd relative abundance values 
+      for(i in 1:100){
+        spp.tbd.wtd[i] <- exp(alpha0 + beta.prey[1]*0 + beta.prey[2]*0 + beta.prey[3]*newcovs[i,3])
+      }
+      
+      } ")
