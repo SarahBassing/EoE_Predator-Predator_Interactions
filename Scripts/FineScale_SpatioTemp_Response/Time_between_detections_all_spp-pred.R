f5c18a87731ce5069a6f6eb217cb867d6909d437
Sarah Bassing <sarah.bassing@gmail.com>
2023-09-18 13:28:17 -0700

    save cleaned up version for publication

--- Scripts/FineScale_SpatioTemp_Response/Time_between_detections_all_spp-pred.R
+++ Scripts/FineScale_SpatioTemp_Response/Time_between_detections_all_spp-pred.R
@@ -74,6 +74,19 @@
   eoe20s_allM <- eoe20s_allM_new
   eoe21s_allM <- eoe21s_allM_new
   
+  #' #'  Trimmed down version for publication
+  #' dets_smr20 <- eoe20s_allM_new %>%
+  #'   dplyr::select(c("NewLocationID", "File", "Date", "Time", "posix_date_time", 
+  #'                   "Human", "Wildlife", "Livestock", "Pet_pack_horse", "Vehicle", "Empty", 
+  #'                   "Species", "Count", "Gmu"))
+  #' dets_smr21 <- eoe21s_allM_new  %>%
+  #'   dplyr::select(c("NewLocationID", "File", "Date", "Time", "posix_date_time", 
+  #'                   "Human", "Wildlife", "Livestock", "Pet_pack_horse", "Vehicle", "Empty", 
+  #'                   "Species", "Count", "Gmu"))
+  #' 
+  #' save(dets_smr20, file = "C:/Users/sbassing/OneDrive - University of Idaho/Repositories/Idaho_Predator_CoOccurrence/Data/dets_smr20.RData")
+  #' save(dets_smr21, file = "C:/Users/sbassing/OneDrive - University of Idaho/Repositories/Idaho_Predator_CoOccurrence/Data/dets_smr21.RData")
+  
   #'  ------------------------
   ####  Filter detection data  ####
   #'  ------------------------
