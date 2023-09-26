17715fb656f7f3db5fdd80473b6d04070c905a29
Sarah Bassing <sarah.bassing@gmail.com>
2023-09-26 14:06:07 -0700

    comment out cleaning steps since only need to do once, update Date format, calcualte RAI metrics for summer 2022 data

--- Scripts/Relative_Abundance/Relative_abundance_indices.R
+++ Scripts/Relative_Abundance/Relative_abundance_indices.R
@@ -21,93 +21,107 @@
   #'  --------------------------------
   ####  Read & format detection data  ####
   #'  --------------------------------
-  #'  Detection data (motion trigger observations only)
-  load("./Data/IDFG camera data/Split datasets/eoe20s_allM_NewLocationID.RData")
-  load("./Data/IDFG camera data/Split datasets/eoe21s_allM_NewLocationID.RData")
-  
   #'  Sequential problem images
   load("./Data/IDFG camera data/Problem images/eoe20s_sequential_probimgs.RData")
   load("./Data/IDFG camera data/Problem images/eoe21s_sequential_probimgs.RData")
+  load("./Data/IDFG camera data/Problem images/eoe22s_sequential_probimgs.RData")
   
   #'  Problem cameras
   load("./Data/IDFG camera data/Problem cams/eoe20s_problem_cams.RData")
   load("./Data/IDFG camera data/Problem cams/eoe21s_problem_cams.RData")
+  load("./Data/IDFG camera data/Problem cams/eoe22s_problem_cams.RData")
   
   #'  Sampling effort data (number of days cameras operation)
   load("./Data/MultiSpp_OccMod_Outputs/Detection_Histories/SamplingEffort_eoe20s.RData")
   load("./Data/MultiSpp_OccMod_Outputs/Detection_Histories/SamplingEffort_eoe21s.RData") 
+  load("./Data/MultiSpp_OccMod_Outputs/Detection_Histories/SamplingEffort_eoe22s.RData") 
   
-  #'  Load corrected species ID and update larger datasets
-  newSppID <- read.csv("./Data/IDFG camera data/questionable_images_doublecheck_SBBupdated.csv") %>%
-    mutate(posix_date_time = as.POSIXct(posix_date_time, format="%m/%d/%Y %H:%M", tz="UTC"), 
-           Year = as.numeric(format(posix_date_time, "%Y"))) %>%
-    distinct() %>%
-    dplyr::select(c("CamID", "NewLocationID", "File", "Location_Relative_Project", "posix_date_time", "Species", "NewSpecies", "Year"))
-  newnewSppID <- read.csv("./Data/IDFG camera data/More questionable IDs_ST_SBB.csv") %>% 
-    mutate(posix_date_time = gsub("<a0>", " ", posix_date_time),
-           posix_date_time = as.POSIXct(posix_date_time, format="%Y-%m-%d %H:%M", tz="UTC"),
-           Year = as.numeric(format(posix_date_time, "%Y"))) %>%
-    dplyr::select(c("CamID", "NewLocationID", "File", "Location_Relative_Project", "posix_date_time", "Species", "NewSpecies", "Year"))
-  newSppID <- rbind(newSppID, newnewSppID) %>% distinct() 
-  #'  Filter by year and only observations with misidentified species
-  newSppID_20s <- filter(newSppID, Year == "2020") %>% dplyr::select(-Year) %>% filter(Species != NewSpecies)
-  newSppID_21s <- filter(newSppID, Year == "2021") %>% dplyr::select(-Year) %>% filter(Species != NewSpecies)
-  #'  Snag unique ID for each image
-  change_sppID_20s <- as.vector(newSppID_20s$Location_Relative_Project)
-  change_sppID_21s <- as.vector(newSppID_21s$Location_Relative_Project)
-  
-  #'  Function to correct species misclassifications
-  remove_obs <- function(dets, prob_images, correctSppID) {
-    #'  Grab all rows that have a misID's species
-    obs_to_remove <- dets[dets$Location_Relative_Project %in% prob_images,]
-    #'  Switch original species ID with correct one
-    newspp <- left_join(obs_to_remove, correctSppID, by = c("CamID", "NewLocationID", "File", "Species")) %>%
-      dplyr::select(-c("Location_Relative_Project.y", "posix_date_time.y")) %>%
-      relocate(NewSpecies, .after = Species) %>%
-      dplyr::select(-Species) %>%
-      rename(Species = "NewSpecies", Location_Relative_Project = "Location_Relative_Project.x", posix_date_time = "posix_date_time.x")
-    #'  Remove these rows from the larger df
-    dets_reduced <- anti_join(dets, obs_to_remove)
-    #'  Replace with observation containing correct species ID
-    dets_updated <- bind_rows(dets_reduced, newspp) %>%
-      #'  Arrange everything back in order
-      arrange(NewLocationID, posix_date_time, File)
-    return(dets_updated)
-  }
-  eoe20s_allM_new <- remove_obs(eoe20s_allM, prob_images = change_sppID_20s, correctSppID = newSppID_20s)
-  eoe21s_allM_new <- remove_obs(eoe21s_allM, prob_images = change_sppID_21s, correctSppID = newSppID_21s)
-  
-  #'  Re-assign
-  eoe20s_allM <- eoe20s_allM_new
-  eoe21s_allM <- eoe21s_allM_new
-  
+  #' #'  -----------------------------------------------------------------
+  #' ####  Initial cleaning to correct known species misidentifications   ####
+  #' #'  -----------------------------------------------------------------
+  #' #'  Detection data (motion trigger observations only)
+  #' load("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe20s_allM_NewLocationID.RData")
+  #' load("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe21s_allM_NewLocationID.RData")
+  #' load("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe22s_allM_NewLocationID.RData")
+  #' 
+  #' #'  Load corrected species ID and update larger datasets
+  #' newSppID <- read.csv("./Data/IDFG camera data/questionable_images_doublecheck_SBBupdated.csv") %>%
+  #'   mutate(posix_date_time = as.POSIXct(posix_date_time, format="%m/%d/%Y %H:%M", tz="UTC"), 
+  #'          Year = as.numeric(format(posix_date_time, "%Y"))) %>%
+  #'   distinct() %>%
+  #'   dplyr::select(c("CamID", "NewLocationID", "File", "Location_Relative_Project", "posix_date_time", "Species", "NewSpecies", "Year"))
+  #' newnewSppID <- read.csv("./Data/IDFG camera data/More questionable IDs_ST_SBB.csv") %>% 
+  #'   mutate(posix_date_time = gsub("<a0>", " ", posix_date_time),
+  #'          posix_date_time = as.POSIXct(posix_date_time, format="%Y-%m-%d %H:%M", tz="UTC"),
+  #'          Year = as.numeric(format(posix_date_time, "%Y"))) %>%
+  #'   dplyr::select(c("CamID", "NewLocationID", "File", "Location_Relative_Project", "posix_date_time", "Species", "NewSpecies", "Year"))
+  #' newSppID <- rbind(newSppID, newnewSppID) %>% distinct() 
+  #' #'  Filter by year and only observations with misidentified species
+  #' newSppID_20s <- filter(newSppID, Year == "2020") %>% dplyr::select(-Year) %>% filter(Species != NewSpecies)
+  #' newSppID_21s <- filter(newSppID, Year == "2021") %>% dplyr::select(-Year) %>% filter(Species != NewSpecies)
+  #' #'  Snag unique ID for each image
+  #' change_sppID_20s <- as.vector(newSppID_20s$Location_Relative_Project)
+  #' change_sppID_21s <- as.vector(newSppID_21s$Location_Relative_Project)
+  #' 
+  #' #'  Function to correct species misclassifications
+  #' remove_obs <- function(dets, prob_images, correctSppID) {
+  #'   #'  Grab all rows that have a misID's species
+  #'   obs_to_remove <- dets[dets$Location_Relative_Project %in% prob_images,]
+  #'   #'  Switch original species ID with correct one
+  #'   newspp <- left_join(obs_to_remove, correctSppID, by = c("CamID", "NewLocationID", "File", "Species")) %>%
+  #'     dplyr::select(-c("Location_Relative_Project.y", "posix_date_time.y")) %>%
+  #'     relocate(NewSpecies, .after = Species) %>%
+  #'     dplyr::select(-Species) %>%
+  #'     rename(Species = "NewSpecies", Location_Relative_Project = "Location_Relative_Project.x", posix_date_time = "posix_date_time.x")
+  #'   #'  Remove these rows from the larger df
+  #'   dets_reduced <- anti_join(dets, obs_to_remove)
+  #'   #'  Replace with observation containing correct species ID
+  #'   dets_updated <- bind_rows(dets_reduced, newspp) %>%
+  #'     #'  Arrange everything back in order
+  #'     arrange(NewLocationID, posix_date_time, File)
+  #'   return(dets_updated)
+  #' }
+  #' eoe20s_allM_new <- remove_obs(eoe20s_allM, prob_images = change_sppID_20s, correctSppID = newSppID_20s)
+  #' eoe21s_allM_new <- remove_obs(eoe21s_allM, prob_images = change_sppID_21s, correctSppID = newSppID_21s)
+  #' 
+  #' #'  Re-assign
+  #' eoe20s_allM <- eoe20s_allM_new
+  #' eoe21s_allM <- eoe21s_allM_new
+  #' 
   #' #'  Save cleaned data
-  #' save(eoe20s_allM, file = paste0("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe20s_allM_", Sys.Date(), ".RData"))
-  #' save(eoe21s_allM, file = paste0("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe21s_allM_", Sys.Date(), ".RData"))
+  #' save(eoe20s_allM, file = paste0("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe20s_allM_NewLocationID_", Sys.Date(), ".RData"))
+  #' save(eoe21s_allM, file = paste0("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe21s_allM_NewLocationID_", Sys.Date(), ".RData"))
+  #' save(eoe22s_allM, file = paste0("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe22s_allM_NewLocationID_", Sys.Date(), ".RData"))
   
   #'  ------------------------
   ####  Filter detection data  ####
   #'  ------------------------
+  #'  Load cleaned data (with corrected species IDs)
+  load("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe20s_allM_NewLocationID_2023-09-26.RData") #2023-09-26 is most recent version
+  load("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe21s_allM_NewLocationID_2023-09-26.RData")
+  load("./Data/IDFG camera data/Split datasets/Updated_EoE_datasets/eoe22s_allM_NewLocationID_2023-09-26.RData")
+  
   #'  1) Remove sequential problem images from larger image set
   thin_detections <- function(dets, seqprobs) {
     skinny_dets <- dets[!(dets$File %in% seqprobs$File),]
     return(skinny_dets)
   }
-  eoe20s_allM_skinny <- thin_detections(eoe20s_allM, eoe_seqprob_20s) #eoe20s_allM_2023-08-09 is most recent version
-  eoe21s_allM_skinny <- thin_detections(eoe21s_allM, eoe_seqprob_21s) #eoe21s_allM_2023-08-09 is most recent version
+  eoe20s_allM_skinny <- thin_detections(eoe20s_allM, eoe_seqprob_20s) 
+  eoe21s_allM_skinny <- thin_detections(eoe21s_allM, eoe_seqprob_21s) 
+  eoe22s_allM_skinny <- thin_detections(eoe22s_allM, eoe_seqprob_21s) 
   
   #'  2) Filter detection data to time period and species of interest
   #'  Species of interest are ungulates, humans/vehicles, and domestic animals
   #'  Time periods of interest determined by IDFG and plotting histograms of when 
   #'  most cameras were active each season (Detection_data_cleaning.R script)
-  detections <- function(dets, start_date, end_date, days_operable) {
-    dets <- dets %>%
+  detections <- function(dat, start_date, end_date, days_operable) {
+    dets <- dat %>%
       dplyr::select("NewLocationID", "CamID", "File", "Location_Relative_Project", 
                     "Date", "Time", "posix_date_time", "TriggerMode",
                     "OpState", "Species", "Vehicle", "Count") %>%
       filter(Species != "none") %>%
       mutate(
-        Date = as.Date(Date, format = "%d-%b-%Y"),
+        Date = as.Date(Date, format = "%Y-%m-%d"), #format = "%d-%b-%Y"
         Time = chron(times = Time),
         Species = ifelse(Vehicle == "TRUE", "human_motorized", Species),
         Category = ifelse(Species == "bobcat" | Species == "bear_black" | 
@@ -139,10 +153,12 @@
   }
   eoe20s_dets <- detections(eoe20s_allM_skinny, start_date = "2020-07-01", end_date = "2020-09-15", days_operable = effort_20s)
   eoe21s_dets <- detections(eoe21s_allM_skinny, start_date = "2021-07-01", end_date = "2021-09-15", days_operable = effort_21s)
+  eoe22s_dets <- detections(eoe22s_allM_skinny, start_date = "2022-07-01", end_date = "2022-09-15", days_operable = effort_22s)
   
   #'  Double check all cameras have sampling effort data
   unique(eoe20s_dets$NewLocationID[is.na(eoe20s_dets$ndays)])
   unique(eoe21s_dets$NewLocationID[is.na(eoe21s_dets$ndays)])
+  unique(eoe22s_dets$NewLocationID[is.na(eoe22s_dets$ndays)])
   
   #'  -----------------------------------------
   ####  Generate independent detection events  ####
@@ -175,8 +191,9 @@
   }
   eoe20s_5min_dets <- unique_detections(eoe20s_dets, elapsed_time = 300) # (5*60 = 300 seconds)
   eoe21s_5min_dets <- unique_detections(eoe21s_dets, elapsed_time = 300)
+  eoe22s_5min_dets <- unique_detections(eoe22s_dets, elapsed_time = 300)
   #'  List 5min-elapsed detection events
-  eoe_5min_list <- list(eoe20s_5min_dets, eoe21s_5min_dets)
+  eoe_5min_list <- list(eoe20s_5min_dets, eoe21s_5min_dets ,eoe22s_5min_dets)
   
   #'  -------------------------------------------------  
   ####  Calculating simple Relative Abundance Indices  ####
@@ -237,13 +254,18 @@
       full_join(trap_nights, by = "NewLocationID") %>%
       mutate(RAI_nimgs = round((n_imgs/ndays) , 3),
              RAI_ndets = round((n_dets/ndays) , 3),
-             RAI_nhrs = round((n_det_hrs/ndays) , 3))
+             RAI_nhrs = round((n_det_hrs/ndays) , 3)) %>%
+      #'  Retain data from cameras where camera was operable for 30 days or more
+      filter(ndays >= 30) %>%
+      #'  Remove sites where camera was operable 30+ days but no wildlife detections occurred
+      filter(!is.na(n_imgs))
     
     return(count_dat)
   }
   eoe20s_RAI <- RAI_metrics(eoe20s_5min_dets, effort_20s)
   eoe21s_RAI <- RAI_metrics(eoe21s_5min_dets, effort_21s)
-  eoe_RAI <- list(eoe20s_RAI, eoe21s_RAI)
+  eoe22s_RAI <- RAI_metrics(eoe22s_5min_dets, effort_22s)
+  eoe_RAI <- list(eoe20s_RAI, eoe21s_RAI, eoe22s_RAI)
   
   #'  Save
   save(eoe_RAI, file = "./Data/Relative abundance data/RAI Phase 2/eoe_RAI.RData")
@@ -288,6 +310,7 @@
   }
   eoe20s_corr <- compare_counts(eoe20s_RAI, eoe_density_list[[1]])
   eoe21s_corr <- compare_counts(eoe21s_RAI, eoe_density_list[[2]])
+  eoe22s_corr <- compare_counts(eoe22s_RAI, eoe_density_list[[3]])
   
   
   
