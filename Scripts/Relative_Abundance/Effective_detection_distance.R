eae6f84345899a83531e02bb58eb43aa232cd3cd
Sarah Bassing <sarah.bassing@gmail.com>
2023-10-02 16:49:27 -0700

    Script to summarize and format effective detection distances

---
+++ Scripts/Relative_Abundance/Effective_detection_distance.R
@@ -0,0 +1,157 @@
+  #'  -------------------------------
+  #'  Effective Detection Distance
+  #'  ID CRU - Predator Interactions
+  #'  Sarah Bassing
+  #'  October 2023
+  #'  -------------------------------
+  #'  Estimate average effective detection distance for each predator and camera
+  #'  setup type (predator vs ungulate) based on measuring distance to animal (m) 
+  #'  in motion triggered camera trap images.
+  #'  -------------------------------
+  
+  #'  Load libraries
+  library(data.table)
+  library(tidyverse)
+  
+  #'  ----------------------
+  ####  Read & format data  ####
+  #'  ----------------------
+  #'  Distance measurements by camera setup type
+  pred_mt <- read_csv("./Data/Relative abundance data/RAI Phase 2/Pred_MT_distance.csv") %>%
+    filter(ReferencePics == FALSE) %>%
+    filter(Species != "marten") %>%
+    mutate(CamID = ifelse(File == "EOE2022S_IDFG2508_20220725_171547_MD_1.JPG", "IDFG2508", CamID),
+           Species = ifelse(Species == "black_bear", "bear_black", Species)) 
+  
+  ung_mt <- read_csv("./Data/Relative abundance data/RAI Phase 2/Ung_MT_distance.csv") %>%
+    filter(ReferencePics == FALSE) %>%
+    filter(Species != "whitetailed_deer") %>%
+    mutate(Species = ifelse(Species == "black_bear", "bear_black", Species))
+  
+  #'  Camera location data
+  cams <- read_csv("./Data/IDFG camera data/cams_eoe_long_Smr2020-2022.csv") %>%
+    dplyr::select(c("NewLocationID", "CamID", "Setup", "Season", "Lat", "Long", "CameraHeight_M", "CameraFacing")) %>%
+    filter(Season != "Wtr20") %>%
+    filter(NewLocationID != "UNKNOWN") %>%
+    filter(CamID != "unknown") %>%
+    group_by(NewLocationID) %>%
+    arrange(Season) %>%
+    slice(n()) %>%
+    ungroup() 
+  
+  #'  Calculated time-in-front-of-camera for each species and camera (Smr22)
+  tifc <- read_csv("./Data/Relative abundance data/RAI Phase 2/eoe_all_22s_fov-time.csv") %>%
+    filter(common_name == "bear_black" | common_name == "bobcat" | common_name == "coyote" | 
+             common_name == "mountain_lion" | common_name == "wolf") %>%
+    mutate(Setup = ifelse(grepl("P", location), "predator", "ungulate"),
+           ) %>%
+    rename("NewLocationID" = "location") %>%
+    rename("Species" = "common_name")
+  
+  #'  ---------------------------------
+  ####  Format and summarize distance  ####
+  #'  ---------------------------------
+  #'  Format so each detected individual per image has a unique row where its
+  #'  unique detection distance is recorded
+  detection_dis_per_individual <- function(cam_dat, dist_dat) {
+    #'  Bind camera and distance data together
+    individual_distances <- full_join(cam_dat, dist_dat, by = "CamID") %>%
+      dplyr::select(c("NewLocationID", "CamID", "Setup", "Season", "Lat", "Long", "File", "CameraHeight_M", "CameraFacing",
+                      "DateTime", "Species", "Count", "Ind1_dist", "Ind2_dist", "Ind3_dist", "Ind4_dist")) %>%
+      #'  Replace Count NAs with 1 (required for uncount function below to work)
+      mutate(Count = ifelse(is.na(Count), 1, Count)) %>%
+      #'  Repeat rows based on count data (i.e., if 2 animals detected, repeat observation once)
+      uncount(Count, .remove = FALSE) %>%
+      mutate(Count = ifelse(is.na(Species), NA, Count)) %>%
+      #'  Group and number each observation within group data so repeat rows have a unique identifier
+      group_by(NewLocationID, File, DateTime) %>%
+      mutate(ind_animal = row_number()) %>%
+      ungroup() %>%
+      #'  Create column where unique detection distance is recorded for each individual 
+      #'  based on how many times each detection was repeated and the number of 
+      #'  individual distances measured for that image
+      mutate(det_dist = ifelse(ind_animal == 1, Ind1_dist, Ind2_dist),
+             det_dist = ifelse(ind_animal == 3, Ind3_dist, det_dist),
+             det_dist = ifelse(ind_animal == 4, Ind4_dist, det_dist)) %>%
+      dplyr::select(-c(File, Ind1_dist, Ind2_dist, Ind3_dist, Ind4_dist))
+    return(individual_distances)
+  }
+  
+  pred_dist <- detection_dis_per_individual(cam_dat = cams[cams$Setup == "predator",], dist_dat = pred_mt)
+  ung_dist <- detection_dis_per_individual(cam_dat = cams[cams$Setup == "ungulate",], dist_dat = ung_mt)
+  
+  #'  Summarize detection distances
+  #'  Note- warnings due to infinite values being created when finding min/max of NAs
+  avg_dist <- function(dist) {
+    #'  Summarize detection distances per species per camera site
+    avg_dist_per_spp_cam <- dist %>%
+      group_by(NewLocationID, Species) %>%
+      summarise(avg_dist = mean(det_dist, na.rm = TRUE),
+                min_dist = min(det_dist, na.rm = TRUE),
+                max_dist = max(det_dist, na.rm = TRUE),
+                n_obs = n()) %>%
+      ungroup() %>%
+      mutate(avg_dist = round(avg_dist, 2), 
+             avg_dist = ifelse(is.na(avg_dist), NA, avg_dist),
+             min_dist = ifelse(is.infinite(min_dist), NA, min_dist),
+             max_dist = ifelse(is.infinite(max_dist), NA, max_dist)) 
+      
+    #'  Summarize detection distances per species
+    avg_dist_per_spp <- dist %>%
+      filter(!is.na(Species)) %>%
+      group_by(Species) %>%
+      summarise(avg_dist = mean(det_dist, na.rm = TRUE),
+                min_dist = min(det_dist, na.rm = TRUE),
+                max_dist = max(det_dist, na.rm = TRUE),
+                n_obs = n()) %>%
+      ungroup() %>%
+      mutate(avg_dist = round(avg_dist, 2))
+    print(avg_dist_per_spp)
+    
+    avg_dist <- list(avg_dist_per_spp_cam, avg_dist_per_spp)
+    return(avg_dist)
+  }
+  pred_dist_avg <- avg_dist(pred_dist)
+  ung_dist_avg <- avg_dist(ung_dist)
+  
+  
+  #'  Generate single df containing TIFC and detection distance data
+  tifc_edd <- function(tifc, dist) {
+    #'  Bind TIFC and detection distance data together
+    #'  Note- can have distances for species at sites where total_duration = 0 in tifc df
+    #'  b/c distance measurements include observations from outside July 1 - Sept 15 time frame
+    merge_dat <- left_join(tifc, dist[[1]], by = c("NewLocationID", "Species"))
+    
+    #'  Identify which TIFC observations are missing detection data and fill in with
+    #'  species averages
+    tifc_with_dist <- merge_dat %>%
+      filter(!is.na(avg_dist))
+    
+    missing_dist <- merge_dat %>%
+      filter(is.na(avg_dist)) %>%
+      dplyr::select(-c(avg_dist, min_dist, max_dist, n_obs)) %>%
+      left_join(pred_dist_avg[[2]], by = "Species")
+    
+    #'  Bind so each species & site specific TIFC observation has detection distance data
+    tifc_edd_full <- rbind(tifc_with_dist, missing_dist) %>%
+      #'  Re-organize to data are ready for Calculate_density.R script
+      arrange(NewLocationID, Species) %>%
+      dplyr::select(-Setup) %>%
+      rename("location" = "NewLocationID") %>%
+      rename("common_name" = "Species")
+    
+    return(tifc_edd_full)
+  }
+  pred_tifc_edd <- tifc_edd(tifc[tifc$Setup == "predator",], dist = pred_dist_avg)
+  ung_tifc_edd <- tifc_edd(tifc[tifc$Setup == "ungulate",], dist = ung_dist_avg)
+  
+  
+  #'  Does detection distance differ by camera setup or habitat type (account for this when applying avg. dist for species with missing dist data)
+  
+  
+  
+  
+  
+  
+
+    
