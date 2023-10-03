457977262551742d0c270b0eef975713e53b1a1e
Sarah Bassing <sarah.bassing@gmail.com>
2023-10-03 11:41:23 -0700

    use edd instead of area_m2 when calculating effort, focus on predators only

--- Scripts/Relative_Abundance/Calculate_denisty.R
+++ Scripts/Relative_Abundance/Calculate_denisty.R
@@ -8,6 +8,8 @@
   #'  described by Huggard et al. 2018, Warbington & Boyce 2020, and Becker et al. 2022.
   #'  Code modified from Becker et al. 2018 repository: https://github.com/mabecker89/tifc-method/tree/main
   #'  
+  #'  Time-in-front-of-camera (TIFC) generated in Calculate_time-in-front-of-camera.R
+  #'  Effective detection distance (EDD) appended to TIFC in Effective_detection_distance.R
   #'  Sequential probimgs generated in Detection_data_cleaning.R (Data_Formatting folder)
   #'  Sampling effort data generated in Detection_histories_for_occmod.R (MultiSpp_OccMod folder)
   #'  Leave probabilities taken directly from Becker et al. (2018)
@@ -24,32 +26,36 @@
   ####  Load and format input data  ####
   #'  -------------------------------  
   #'  TIFC data
-  load("./Data/Relative abundance data/RAI Phase 2/eoe_all_fov-time.RData")
+  load("./Data/Relative abundance data/RAI Phase 2/eoe_all_fov-time_edd.RData")
+  # load("./Data/Relative abundance data/RAI Phase 2/eoe_all_fov-time.RData")
   # df_tt_full_20s <- read_csv("./Data/Relative abundance data/RAI Phase 2/eoe_all_20s_fov-time.csv") 
   # df_tt_full_21s <- read_csv("./Data/Relative abundance data/RAI Phase 2/eoe_all_21s_fov-time.csv")
   # df_tt_full_22s <- read_csv("./Data/Relative abundance data/RAI Phase 2/eoe_all_22s_fov-time.csv")
   
-  #'  Filter to focal speices
+  #'  Filter to focal species
   focal_spp <- function(df_tt_full) {
     df_tt_full <- df_tt_full %>%
+      # filter(common_name == "bear_black" | common_name == "bobcat" | common_name == "coyote" |
+      #          common_name == "elk" | common_name == "moose" | common_name == "mountain_lion" |
+      #          common_name == "muledeer" | common_name == "rabbit_hare" | 
+      #          common_name == "whitetaileddeer" | common_name == "wolf")
       filter(common_name == "bear_black" | common_name == "bobcat" | common_name == "coyote" |
-               common_name == "elk" | common_name == "moose" | common_name == "mountain_lion" |
-               common_name == "muledeer" | common_name == "rabbit_hare" | 
-               common_name == "whitetaileddeer" | common_name == "wolf")
+              common_name == "mountain_lion" | common_name == "wolf") %>%
+      rename("detdist" = "avg_dist")
     return(df_tt_full)
   }
   df_tt_full_20s <- focal_spp(tt_list[[1]])
   df_tt_full_21s <- focal_spp(tt_list[[2]])
   df_tt_full_22s <- focal_spp(tt_list[[3]])
   
-  #'  Load effective detection distance (EDD) 
-  #'  Currently using detection zone measured at camera by IDFG. Area (m2) measured 
-  #'  using walk test, flagging, or other methods to estimate detection zone
-  #'  These values are not corrected for habitat type or species
-  load("./Data/Relative abundance data/RAI Phase 2/area_m2.RData")
-  area_m2_20s <- area_m2[[1]] %>% rename("location" = "NewLocationID") %>% rename("edd_area_m2" = "Area_M2")
-  area_m2_21s <- area_m2[[2]] %>% rename("location" = "NewLocationID") %>% rename("edd_area_m2" = "Area_M2")
-  area_m2_22s <- area_m2[[3]] %>% rename("location" = "NewLocationID") %>% rename("edd_area_m2" = "Area_M2")
+  #' #'  Load effective detection distance (EDD) 
+  #' #'  Currently using detection zone measured at camera by IDFG. Area (m2) measured 
+  #' #'  using walk test, flagging, or other methods to estimate detection zone
+  #' #'  These values are not corrected for habitat type or species
+  #' load("./Data/Relative abundance data/RAI Phase 2/area_m2.RData")
+  #' area_m2_20s <- area_m2[[1]] %>% rename("location" = "NewLocationID") %>% rename("edd_area_m2" = "Area_M2")
+  #' area_m2_21s <- area_m2[[2]] %>% rename("location" = "NewLocationID") %>% rename("edd_area_m2" = "Area_M2")
+  #' area_m2_22s <- area_m2[[3]] %>% rename("location" = "NewLocationID") %>% rename("edd_area_m2" = "Area_M2")
   
   #'  Update species names for "distance groups" based on Becker et al. (2018) classifications
   spp_20s <- as.data.frame(unique(df_tt_full_20s$common_name)); names(spp_20s) <- "common_name"
@@ -58,8 +64,9 @@
   df_dist_groups_20s <- arrange(spp_20s, common_name)
   df_dist_groups_21s <- arrange(spp_21s, common_name)
   df_dist_groups_22s <- arrange(spp_22s, common_name)
-  dist_group <- c("Bear", "Lynx", "CoyoteFox","Elk", "BigForestUngulates", "BigForestCarnivores", 
-                  "Muledeer", "SmallForest", "WTDeer", "BigForestCarnivores")
+  # dist_group <- c("Bear", "Lynx", "CoyoteFox","Elk", "BigForestUngulates", "BigForestCarnivores", 
+  #                 "Muledeer", "SmallForest", "WTDeer", "BigForestCarnivores")
+  dist_group <- c("Bear", "Lynx", "CoyoteFox","BigForestCarnivores", "BigForestCarnivores")
   df_dist_groups_20s$dist_group <- dist_group
   df_dist_groups_21s$dist_group <- dist_group
   df_dist_groups_22s$dist_group <- dist_group
@@ -87,8 +94,8 @@
   #'  Join all ingredients ('ing') required to calculate density
   all_ing <- function(df_tt_full, df_dist_groups, area_m2) {
     df_dens_ing <- df_tt_full %>%
-      left_join(df_dist_groups, by = "common_name") %>%
-      left_join(area_m2, by = "location") #%>%
+      left_join(df_dist_groups, by = "common_name") #%>%
+      # left_join(area_m2, by = "location") #%>%
     # left_join(df_detdist, by = c("location", "dist_group")) %>%
     # # Remove random species (mostly birds) 
     # filter(!is.na(detdist))
@@ -99,12 +106,12 @@
   df_dens_ing_22s <- all_ing(df_tt_full_22s, df_dist_groups = df_dist_groups_22s, area_m2 = area_m2_22s)
   
   
-  #####  Step 2. Calculate density  #####
+  #####  Step 2. Calculate density  #####         # should I be using mean EDD or max EDD when calculating effort????
   #'  -----------------------------------
   calc_density <- function(df_dens_ing) {
     df_density <- df_dens_ing %>%
-      # mutate(effort = total_season_days * (detdist ^ 2 * pi * (cam_fov_angle / 360)) / 100,
-      mutate(effort = total_season_days * (edd_area_m2 * pi * (cam_fov_angle / 360)) / 100,
+      mutate(effort = total_season_days * (detdist ^ 2 * pi * (cam_fov_angle / 360)) / 100,
+      # mutate(effort = total_season_days * (edd_area_m2 * pi * (cam_fov_angle / 360)) / 100,
              #'  Catch per unit effort
              cpue = total_duration / effort,
              #'  Catch per unit effort in km2
@@ -119,12 +126,14 @@
   #'  Bind into a single data frame
   df_density <- rbind(df_density_20s, df_density_21s, df_density_22s) %>%
     #'  Add GMU to dataset
-    mutate(gmu = sub("_.*", "", location)) %>%
-    relocate(gmu, .after = "season")
+    mutate(gmu = sub("_.*", "", location),
+           setup = ifelse(grepl("P", location), "predator", "ungulate")) %>%
+    relocate(gmu, .after = "season") %>%
+    relocate(setup, .after = "gmu")
   
   
   #'  SAVE! 
-  write_csv(df_density, "./Data/Relative abundance data/RAI Phase 2/eoe_all_years_density.csv")
+  write_csv(df_density, "./Data/Relative abundance data/RAI Phase 2/eoe_all_years_density_edd_predonly.csv")
   
   #'  Summary stats for density estimates
   density_stats <- df_density %>%
@@ -135,5 +144,17 @@
               se_density_100km2 = round((sd(cpue_100km2, na.rm = TRUE)/sqrt(nrow(.))), 2)) %>%
     ungroup() %>%
     arrange(gmu, mean_density_km2)
+  print(density_stats)
   
-  write_csv(density_stats, "./Data/Relative abundance data/RAI Phase 2/tifc_density_stats.csv")
+  write_csv(density_stats, "./Data/Relative abundance data/RAI Phase 2/tifc_density_stats_edd_predonly.csv")
+
+  #'  Next challenges...
+  #'  1. Bootstrap estimates so we have standard errors for camera-level density
+  #'  2. Do we trust EDD based on camera setups?
+  #'  3. If yes, probably need EDD for prey too so effort is consistent across species
+  #'  4. Which density metric to use (cpue, cpue km2, cpue 100km2)?
+  #'  5. How to remove investigation images for bears across entire dataset?
+  #'  6. Detection rate vs TIFC as response variable (both are going to be indices of abund/density)?
+  #'  ALSO- should I be using mean EDD or max EDD when calculating effort????
+  
+  
