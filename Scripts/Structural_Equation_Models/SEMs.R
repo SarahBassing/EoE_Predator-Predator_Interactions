0ca8e64da36241337c6a408cf385684538ec1334
SarahBassing <sarah.bassing@gmail.com>
2024-01-12 10:14:00 -0800

    Start of script to create SEMs with local abund data

---
+++ Scripts/Structural_Equation_Models/SEMs.R
@@ -0,0 +1,14 @@
+  #'  --------------------------------
+  #'  Structural Equation Models
+  #'  ID CRU - Predator Interactions
+  #'  Sarah Bassing
+  #'  January 2024
+  #'  --------------------------------
+  #'  Run SEMs
+  
+  #'  Load libraries
+  library(piecewiseSEM)
+  library(tidyverse)
+  
+  #'  Load RN model local abundance estiamtes
+  load("./Outputs/Relative_Abundance/RN_model/RN_abundance.RData")
