b3e16e3e377711bbf2a2b1a13c875c2a1e20bab8
SarahBassing <sarah.bassing@gmail.com>
2024-07-15 14:24:29 -0700

    update linear model back to elev, no footprint or tri

--- Scripts/MultiSpp_OccMod/JAGS code/JAGS_code_psi(global)_psix(global)_p(setup_effort)_coybob.R
+++ Scripts/MultiSpp_OccMod/JAGS code/JAGS_code_psi(global)_psix(global)_p(setup_effort)_coybob.R
@@ -130,7 +130,7 @@
               
           #'  ...for states Spp1, Spp2
           #'  Covariate order: Intercept[1] + Setup[2] + Year[5] + Elevation[3] + Forest[4] + White-tailed deer[10] + Lagomorph[11] + SppDiversity[6] # + Footprint[16] + TRI[17]
-          psiSpp1[i] <- betaSpp1[1]*psi_cov[i,1] + betaSpp1[2]*psi_cov[i,2] + betaSpp1[3]*psi_cov[i,5] + betaSpp1[4]*psi_cov[i,3] + betaSpp1[5]*psi_cov[i,4] + betaSpp1[6]*psi_cov[i,10] + betaSpp1[7]*psi_cov[i,11] + betaSpp1[8]*psi_cov[i,6]# + betaSpp1[9]*psi_cov[i,16] + betaSpp1[4]*psi_cov[i,17
+          psiSpp1[i] <- betaSpp1[1]*psi_cov[i,1] + betaSpp1[2]*psi_cov[i,2] + betaSpp1[3]*psi_cov[i,5] + betaSpp1[4]*psi_cov[i,3] + betaSpp1[5]*psi_cov[i,4] + betaSpp1[6]*psi_cov[i,10] + betaSpp1[7]*psi_cov[i,11] + betaSpp1[8]*psi_cov[i,6]# + betaSpp1[9]*psi_cov[i,16] + betaSpp1[4]*psi_cov[i,17]
           psiSpp2[i] <- betaSpp2[1]*psi_cov[i,1] + betaSpp2[2]*psi_cov[i,2] + betaSpp2[3]*psi_cov[i,5] + betaSpp2[4]*psi_cov[i,3] + betaSpp2[5]*psi_cov[i,4] + betaSpp2[6]*psi_cov[i,10] + betaSpp2[7]*psi_cov[i,11] + betaSpp2[8]*psi_cov[i,6]# + betaSpp2[9]*psi_cov[i,16] + betaSpp2[4]*psi_cov[i,17]
           
           #'  ...for state Spp12
