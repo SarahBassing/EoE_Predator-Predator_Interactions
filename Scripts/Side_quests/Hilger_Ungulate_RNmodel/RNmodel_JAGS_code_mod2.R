87f9be52602526d4cdd8dc9dbca820f2a0a8a9ae
SarahBassing <sarah.bassing@gmail.com>
2024-08-05 16:14:17 -0700

    clean up script

--- Scripts/Side_quests/Hilger_Ungulate_RNmodel/RNmodel_JAGS_code_mod2.R
+++ Scripts/Side_quests/Hilger_Ungulate_RNmodel/RNmodel_JAGS_code_mod2.R
@@ -22,86 +22,86 @@
   #'  -------------------------------
   
   cat(file = './Outputs/Hilger_RNmodel/RNmodel_JAGS_code_mod2.txt', "
-          model{
+      model{
           
-          #'  Define priors
-          #'  -------------
-          #'  Abundance priors
-          beta0 ~ dunif(-10, 10)      # Abundance intercept
-          mean.lambda <- exp(beta0)   # Mean lambda for GMU10A
+        #'  Define priors
+        #'  -------------
+        #'  Abundance priors
+        beta0 ~ dunif(-10, 10)      # Abundance intercept
+        mean.lambda <- exp(beta0)   # Mean lambda for GMU10A
           
-          #'  Categorical effect for GMU needs multiple beta1 coefficients
-          beta1[1] <- 0
-          for(gmu in 2:ngmu) {
-            beta1[gmu] ~ dnorm(0, 0.001)
-          }
+        #'  Categorical effect for GMU needs multiple beta1 coefficients
+        beta1[1] <- 0
+        for(gmu in 2:ngmu) {
+          beta1[gmu] ~ dnorm(0, 0.001)
+        }
           
-          #'  Continuous effects for elevation and forest cover
-          beta2 ~ dnorm(0, 0.001)
-          beta3 ~ dnorm(0, 0.001)
-          # beta4 ~ dnorm(0, 0.001)
-          # beta5 ~ dnorm(0, 0.001)
+        #'  Continuous effects for elevation and forest cover
+        beta2 ~ dnorm(0, 0.001)
+        beta3 ~ dnorm(0, 0.001)
+        # beta4 ~ dnorm(0, 0.001)
+        # beta5 ~ dnorm(0, 0.001)
           
-          #'  Detection priors
-          mean.r ~ dunif(0, 1)        # Detection intercept (on probability scale)
-          alpha0 <- logit(mean.r)     # Detection intercept (on logit scale)
+        #'  Detection priors
+        mean.r ~ dunif(0, 1)        # Detection intercept (on probability scale)
+        alpha0 <- logit(mean.r)     # Detection intercept (on logit scale)
           
-          #'  Categorical effect for camera setup needs multiple alpha1 coefficients
-          alpha1[1] <- 0
-          for(cam in 2:nsets) {
-            alpha1[cam] ~ dnorm(0, 0.001)
-          }
+        #'  Categorical effect for camera setup needs multiple alpha1 coefficients
+        alpha1[1] <- 0
+        for(cam in 2:nsets) {
+          alpha1[cam] ~ dnorm(0, 0.001)
+        }
           
           
-          #'  Define likelihood
-          #'  -----------------
-          #'  Latent state (abundance)
-          for(i in 1:nsites){
-            N[i] ~ dpois(lambda[i])
-            lambda[i] <- exp(beta0 + beta1[gmu[i]]) + beta2*elev[i] + beta3*forest[i])
+        #'  Define likelihood
+        #'  -----------------
+        #'  Latent state (abundance)
+        for(i in 1:nsites){
+          N[i] ~ dpois(lambda[i])
+          lambda[i] <- exp(beta0 + beta1[gmu[i]] + beta2*elev[i] + beta3*forest[i])
             
-            #'  Detection state
-            for(j in 1:nsurveys){
-              y[i,j] ~ dbern(p[i,j])
-              p[i,j] <- 1 - pow((1 - r[i,j]), N[i])
-              logit(r[i,j]) <- alpha0 + alpha1[setup[i]]
-            }
+          #'  Detection state
+          for(j in 1:nsurveys){
+            y[i,j] ~ dbern(p[i,j])
+            p[i,j] <- 1 - pow((1 - r[i,j]), N[i])
+            logit(r[i,j]) <- alpha0 + alpha1[setup[i]]
           }
+        }
           
-          #'  Derived parameters
-          #'  ------------------
-          #'  Mean lambda per GMU at road sites
-          for(gmu in 1:ngmu) {
-            lambdaGMU[gmu] <- exp(beta0 + beta1[gmu] + beta2*elev[i] + beta3*forest[i])
-          }
+        #'  Derived parameters
+        #'  ------------------
+        #'  Mean lambda per GMU at road sites
+        for(gmu in 1:ngmu) {
+          lambdaGMU[gmu] <- exp(beta0 + beta1[gmu])
+        }
   
-          #'  Mean lambda averaged across GMUs
-          mu.lambda <- mean(lambdaGMU[])
+        #'  Mean lambda averaged across GMUs
+        mu.lambda <- mean(lambdaGMU[])
           
-          #'  Total abundance across camera sites
-          totalN <- sum(N[])
+        #'  Total abundance across camera sites
+        totalN <- sum(N[])
           
-          #'  Total sites occupied (N > 0)
-          for(i in 1:nsites) {
-            occupied[i] <- ifelse(N[i] > 0, 1, 0)
-          }
-          occSites <- sum(occupied[])
+        #'  Total sites occupied (N > 0)
+        for(i in 1:nsites) {
+          occupied[i] <- ifelse(N[i] > 0, 1, 0)
+        }
+        occSites <- sum(occupied[])
     
-          #'  Mean occupancy probability
-          mean.psi <- 1 - exp(-mu.lambda)
+        #'  Mean occupancy probability
+        mean.psi <- 1 - exp(-mu.lambda)
      
-          #'  Mean per-individual detection probability (r) per camera setup
-          for(cam in 1:nsets) {
-            rSetup[cam] <- 1/(1 + exp(-(alpha0 + alpha1[cam])))
-          }
-          #'  per-individual detection probability (r) averaged across all camera setups 
-          mu.r <- mean(rSetup[])
+        #'  Mean per-individual detection probability (r) per camera setup
+        for(cam in 1:nsets) {
+          rSetup[cam] <- 1/(1 + exp(-(alpha0 + alpha1[cam])))
+        }
+        #'  per-individual detection probability (r) averaged across all camera setups 
+        mu.r <- mean(rSetup[])
     
-          #'  Mean detection probability (p)
-          for(i in 1:nsites) {
-            p.occasion[i] <- mean(p[i,])
-          }
-          mean.p <- mean(p.occasion[])
+        #'  Mean detection probability (p)
+        for(i in 1:nsites) {
+          p.occasion[i] <- mean(p[i,])
+        }
+        mean.p <- mean(p.occasion[])
           
-          }
-          ")
+        }
+        ")
