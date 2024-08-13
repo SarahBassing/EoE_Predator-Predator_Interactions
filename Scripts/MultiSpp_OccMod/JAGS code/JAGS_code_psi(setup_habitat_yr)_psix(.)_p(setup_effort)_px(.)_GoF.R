701086b79a6a7d8d987bfd2222728ec206892733
SarahBassing <sarah.bassing@gmail.com>
2024-08-13 09:53:05 -0700

    Script for Habx with co-dete model, including GoF with co-det

---
+++ Scripts/MultiSpp_OccMod/JAGS code/JAGS_code_psi(setup_habitat_yr)_psix(.)_p(setup_effort)_px(.)_GoF.R
@@ -0,0 +1,230 @@
+  #'  ------------------------------------
+  #'  Habitat model, with species interactions on psi & p
+  #'  ID CRU - Predator Interactions
+  #'  Sarah Bassing
+  #'  March 2023
+  #'  ------------------------------------
+  #'  Model to test whether predator occurrence is influenced by basic habitat 
+  #'  features. Allows species co-occurrence to be non-independent.
+  #'  ------------------------------------
+  
+  cat(file = './Outputs/MultiSpp_OccMod_Outputs/JAGS_output/JAGS_code_psi(setup_habitat_yr)_psix(.)_p(setup_effort)_px(.)_GoF.txt', "
+      model{
+          
+        #### Define Priors  ####
+        #'  ================
+        #'  Priors for parameters of interest 
+        #'  Intercepts and slopes for linear models associated with each natural parameter
+        
+        #'  First order occupancy intercerpts (psi) 
+        betaSpp1[1] <- logit(mean.psiSpp1)           
+        betaSpp2[1] <- logit(mean.psiSpp2)
+        mean.psiSpp1 ~ dunif(0, 1)               
+        mean.psiSpp2 ~ dunif(0, 1)
+            
+        #'  First order occupancy slopes (psi)
+        for(fo_psi in 2:6){                         
+          betaSpp1[fo_psi] ~ dnorm(0, 0.1)
+          betaSpp2[fo_psi] ~ dnorm(0, 0.1)
+        }
+      
+        #'  Second order occupancy intercept (psix)
+        betaSpp12[1] ~ dnorm(0, 0.1)
+        
+        #'  First order detection intercepts (rho)
+        alphaSpp1[1] <- logit(mean.pSpp1)           
+        alphaSpp2[1] <- logit(mean.pSpp2)
+        mean.pSpp1 ~ dunif(0, 1)                    
+        mean.pSpp2 ~ dunif(0, 1)
+         
+        #'  First order detection slopes (rho)   
+        for(fo_rho in 2:3){                         
+          alphaSpp1[fo_rho] ~ dnorm(0, 0.1)  
+          alphaSpp2[fo_rho] ~ dnorm(0, 0.1)
+        }
+      
+        #'  Second order detection priors (rho)
+        alphaSpp12[1] ~ dnorm(0, 0.1)
+        alphaSpp21[1] <- 0
+        
+        ####  Define Likelihood  ####
+        #'  =====================
+        
+        #'  1. Set up basic hierarchical model
+        #'  Latent state and observation processes uses a Categorical distribution 
+        #'  since this is essentially a multi-state occupancy model.
+        
+        #'  Latent state model
+        #'  ------------------
+        #'  For each site, true occupancy (z) is drawn from a categorical distribution
+        #'  with 4 mututally exclusive occupancy probabilities
+            
+        for(i in 1:nsites) {
+          z[i] ~ dcat(lsv[i, (1:ncat)])
+          # z.sim[i] ~ dcat(lsv[i, (1:ncat)])
+        }
+          
+        #'  Observation model
+        #'  -----------------
+        #'  For each site and survey occasion, the deteciton data are drawn from a
+        #'  categorical distribution with 4 latent states (z)
+          
+        for(i in 1:nsites) {
+          for(j in 1:nsurveys) {
+            y[i,j] ~ dcat(rdm[i, j, (1:ncat), z[i]])
+            
+            #'  Draw a replicate data set under fitted model
+            y.sim[i,j] ~ dcat(rdm[i, j, (1:ncat), z[i]]) #z.sim[i]
+
+            #'  Derived parameters for Goodness-of-Fit check
+            y2[i,j] <- y[i,j]
+            yrep2[i,j] <- y.sim[i,j]
+
+            #' Seperate out by species
+            y_A[i,j] <- ifelse(y2[i,j]==2 || y2[i,j]==4, 1, 0)
+            y_B[i,j] <- ifelse(y2[i,j]==3 || y2[i,j]==4, 1, 0)
+
+            yrep_A[i,j] <- ifelse(yrep2[i,j]==2 || yrep2[i,j]==4, 1, 0)
+            yrep_B[i,j] <- ifelse(yrep2[i,j]==3 || yrep2[i,j]==4, 1, 0)
+          }
+        }
+        
+        #'  Compute observed z matrix for observed and replicated data
+        for(i in 1:nsites) {
+          zobs_A[i] <- max(y_A[i, ])
+          zobsrep_A[i] <- max(yrep_A[i, ]) # For replicated data
+
+          zobs_B[i] <- max(y_B[i,])       # For observed data
+          zobsrep_B[i] <- max(yrep_B[i,]) # For replicated data
+
+          z_A[i] <- ifelse(z[i]==2 || z[i]==4, 1, 0)
+          z_B[i] <- ifelse(z[i]==3 || z[i]==4, 1, 0)
+
+          #'  Det. frequencies for observed and replicated data
+          detfreq_A[i] <- sum(y_A[i,])
+          detfreqrep_A[i] <- sum(yrep_A[i,])
+
+          detfreq_B[i] <- sum(y_B[i,])
+          detfreqrep_B[i] <- sum(yrep_B[i,])
+
+          #'  Expected detection frequencies under the model
+          for (j in 1:nsurveys){
+            #'  Normalized detection probability for Spp1
+            pA[i,j] <- rdm[i, j, 2, 4]/(1 + rdm[i, j, 2, 4] + rdm[i, j, 4, 4]) # normalize rmd
+            #'  Normalized deteciton probability for Spp2
+            pB[i,j] <- rdm[i, j, 3, 4]/(1 + rdm[i, j, 3, 4] + rdm[i, j, 4, 4]) # nromalize rmd
+            #'  Normalized detection probability for Spp1 AND Spp2 (accounts for co-detection)
+            pAB[i,j] <- rdm[i, j, 4, 4]/(1 + rdm[i, j, 2, 4] + rdm[i, j, 3, 4] + rdm[i, j, 4, 4])
+            #'  Normalized detection probability for Spp1 OR Spp1 with Spp2 
+            pSpp1[i,j] <- pA[i,j] + pAB[i,j]
+            #'  Normalized deteciton probability for Spp2 OR Spp2 with Spp1
+            pSpp2[i,j] <- pB[i,j] + pAB[i,j]
+            #'  Expected detections given Z and normalized detection probabilities
+            tmp_A[i,j] <- z_A[i] * pSpp1[i,j]
+            tmp_B[i,j] <- z_B[i] * pSpp2[i,j]
+            # tmp_A[i,j] <- z_A[i] * rdm[i, j, 2, 2]
+            # tmp_B[i,j] <- z_B[i] * rdm[i, j, 3, 3]
+          }
+
+          E_A[i] <- sum(tmp_A[i,])     # Expected number of detections for A
+          E_B[i] <- sum(tmp_B[i,])     # Expected number of detections for B
+
+          #'  Chi-square and Freeman-Tukey discrepancy measures
+          #'  ..... for actual data
+          x2_A[i] <- pow((detfreq_A[i] - E_A[i]), 2) / (E_A[i] + 0.0001)
+          x2_B[i] <- pow((detfreq_B[i] - E_B[i]), 2) / (E_B[i] + 0.0001)
+
+          ft_A[i] <- pow((sqrt(detfreq_A[i]) - sqrt(E_A[i])), 2)
+          ft_B[i] <- pow((sqrt(detfreq_B[i]) - sqrt(E_B[i])), 2)
+
+          #'  ..... for replicated data set
+          x2rep_A[i] <- pow((detfreqrep_A[i] - E_A[i]), 2) / (E_A[i] + 0.0001)
+          x2rep_B[i] <- pow((detfreqrep_B[i] - E_B[i]), 2) / (E_B[i] + 0.0001)
+
+          ftrep_A[i] <- pow((detfreqrep_A[i]) - sqrt(E_A[i]), 2)
+          ftrep_B[i] <- pow((detfreqrep_B[i]) - sqrt(E_B[i]), 2)
+        }
+
+        #'  Add up overall test statistic and compute fit stat ratio
+        chi2.obs_A <- sum(x2_A[])
+        chi2.obs_B <- sum(x2_B[])
+
+        ft.obs_A <- sum(ft_A[])
+        ft.obs_B <- sum(ft_B[])
+
+        chi2.sim_A <- sum(x2rep_A[])
+        chi2.sim_B <- sum(x2rep_B[])
+
+        ft.sim_A <- ftrep_A[]
+        ft.sim_B <- ftrep_B[]
+          
+        #'  2. Define arrays containing cell probabilities for categorical distributions
+              
+        for(i in 1:nsites) {
+          #'  Probabilities for each latent state (z), held in latent state vector (lsv)
+          lsv[i, 1] <- 1                   # Unoccupied
+          lsv[i, 2] <- exp(psiSpp1[i])     # Pr(Spp1 present)
+          lsv[i, 3] <- exp(psiSpp2[i])     # Pr(Spp2 present)
+          lsv[i, 4] <- exp(psiSpp12[i])    # Pr(Spp1 & Spp2 present)
+         
+          for(j in 1:nsurveys) {
+            #'  Probabilities for each detection array, held in rho detection matrix (rdm) 
+            #'  where OS = observed state, TS = true state and each row sums to 1. 
+            #'  Exponentiating log odds so rdm holds estimates on probability scale.
+            #'  Reminder - this model assumes NO false positives in the data so
+            #'  probability is 0 when OS x TS combinations are not possible.
+            #'  Mmmk don't freak out over this section!
+            #'  Example 1: when only Spp1 is observed and only Spp1 is truly 
+            #'  present, the detection probability is rhoSpp1.
+            #'  Example 2: when only Spp1 is observed by in reality Spp1 & Spp2
+            #'  are truly present, the detection probability is rhoSpp12
+            #'  True state = unoccupied (z = 1 --> 00)
+            rdm[i, j, 1, 1] <- 1 # ------------------------------------ OS = unoccupied
+            rdm[i, j, 2, 1] <- 0 # ------------------------------------ OS = Spp1 present
+            rdm[i, j, 3, 1] <- 0 # ------------------------------------ OS = Spp2 present
+            rdm[i, j, 4, 1] <- 0 # ------------------------------------ OS = Spp12 present
+            #'  True state = Spp1 present (z = 2 --> 10)
+            rdm[i, j, 1, 2] <- 1 # ------------------------------------ OS = unoccupied
+            rdm[i, j, 2, 2] <- exp(rhoSpp1[i, j]) # ------------------- OS = Spp1 present
+            rdm[i, j, 3, 2] <- 0 # ------------------------------------ OS = Spp2 present
+            rdm[i, j, 4, 2] <- 0 # ------------------------------------ OS = Spp12 present
+            #'  True state = Spp2 present (z = 3 --> 01)
+            rdm[i, j, 1, 3] <- 1 # ------------------------------------ OS = unoccupied
+            rdm[i, j, 2, 3] <- 0 # ------------------------------------ OS = Spp1 present
+            rdm[i, j, 3, 3] <- exp(rhoSpp2[i, j]) # ------------------- OS = Spp2 present
+            rdm[i, j, 4, 3] <- 0 # ------------------------------------ OS = Spp12 present
+            #'  Probability of detecting both species together (non-independent detection)
+            #'  True state = Spp1 & Spp2 present (z = 4 --> 11)
+            rdm[i, j, 1, 4] <- 1 # ------------------------------------ OS = unoccupied
+            rdm[i, j, 2, 4] <- exp(rhoSpp1[i, j])  # ------------------ OS = Spp1 present
+            rdm[i, j, 3, 4] <- exp(rhoSpp2[i, j])  # ------------------ OS = Spp2 present
+            rdm[i, j, 4, 4] <- exp(rhoSpp12[i, j]) # ------------------ OS = Spp12 present
+          }
+              
+          #'  3. Define linear models for each fundamental parameter that governs the cell probs
+          #'  These are my natural parameters (f1, f2, f12)!
+          #'  Linear models for the occupancy parameters on the logit scale
+              
+          #'  ...for states Spp1, Spp2
+          #'  Covariate order: Intercept[1] + Setup[2] + Year[3] + Forest[4] + Elevation[5] + TRI[17]
+          psiSpp1[i] <- betaSpp1[1]*psi_cov[i,1] + betaSpp1[2]*psi_cov[i,2] + betaSpp1[3]*psi_cov[i,3] + betaSpp1[4]*psi_cov[i,4] + betaSpp1[5]*psi_cov[i,5] + betaSpp1[6]*psi_cov[i,17] 
+          psiSpp2[i] <- betaSpp2[1]*psi_cov[i,1] + betaSpp2[2]*psi_cov[i,2] + betaSpp2[3]*psi_cov[i,3] + betaSpp2[4]*psi_cov[i,4] + betaSpp2[5]*psi_cov[i,5] + betaSpp2[6]*psi_cov[i,17] 
+          
+          #'  ...for state Spp12
+          #'  Covariate order: Intercept[1] 
+          psiSpp12[i] <- psiSpp1[i] + psiSpp2[i] + betaSpp12[1]*psi_inxs_cov[i,1] 
+          
+          #'  Baseline linear predictors for detection
+          #'  Covariate order: Intercept[1] + Setup[3] + Sampling Effort[5]
+          for(j in 1:nsurveys) {
+            rhoSpp1[i, j] <- alphaSpp1[1]*rho_cov[i,j,1] + alphaSpp1[2]*rho_cov[i,j,3] + alphaSpp1[3]*rho_cov[i,j,5] 
+            rhoSpp2[i, j] <- alphaSpp2[1]*rho_cov[i,j,1] + alphaSpp2[2]*rho_cov[i,j,3] + alphaSpp2[3]*rho_cov[i,j,5] 
+      
+            #'  Second-order interaction on detection (test for non-independence of detections)
+            #'  rhoSpp21 not used but need as place holder based on how Multi-Spp_OccMod_2spp_with_JAGS.R calls model
+            rhoSpp12[i, j] <- rhoSpp1[i, j] + rhoSpp2[i, j] + alphaSpp12[1]*rho_inxs_cov[i,j,1] 
+            rhoSpp21[i, j] <- alphaSpp21[1]*rho_inxs_cov[i,j,1] 
+          }
+        }
+      }
+      ", fill=TRUE)
