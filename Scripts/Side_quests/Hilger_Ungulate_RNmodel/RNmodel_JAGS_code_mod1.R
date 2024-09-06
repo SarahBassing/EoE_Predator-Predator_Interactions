3945df6a8be51a87d6b5495fa22ab99068665570
SarahBassing <sarah.bassing@gmail.com>
2024-09-06 15:57:32 -0700

    update parameter names and covs

--- Scripts/Side_quests/Hilger_Ungulate_RNmodel/RNmodel_JAGS_code_mod1.R
+++ Scripts/Side_quests/Hilger_Ungulate_RNmodel/RNmodel_JAGS_code_mod1.R
@@ -28,22 +28,22 @@
         #'  -------------
         #'  Abundance priors
         beta0 ~ dunif(-10, 10)      # Abundance intercept
-        mean.lambda <- exp(beta0)   # Mean lambda for GMU10A
+        mean.lambda <- exp(beta0)   # Mean lambda for Year 1
           
-        #'  Categorical effect for year needs multiple beta1 coefficients
-        beta1[1] <- 0
+        #'  Categorical effect for year needs multiple b.year coefficients
+        b.year[1] <- 0
         for(yr in 2:nyear) {
-          beta1[yr] ~ dnorm(0, 0.001)
+          b.year[yr] ~ dnorm(0, 0.001)
         }
           
         #'  Detection priors
         mean.r ~ dunif(0, 1)        # Detection intercept (on probability scale)
         alpha0 <- logit(mean.r)     # Detection intercept (on logit scale)
           
-        #'  Categorical effect for camera setup needs multiple alpha1 coefficients
-        alpha1[1] <- 0
+        #'  Categorical effect for camera setup needs multiple a.setup coefficients
+        a.setup[1] <- 0
         for(cam in 2:nsets) {
-          alpha1[cam] ~ dnorm(0, 0.001)
+          a.setup[cam] ~ dnorm(0, 0.001)
         }
           
 
@@ -52,41 +52,26 @@
         #'  Latent state (abundance)
         for(i in 1:nsites){
           N[i] ~ dpois(lambda[i])
-          lambda[i] <- exp(beta0 + beta1[year[i]]) 
+          lambda[i] <- exp(beta0 + b.year[year[i]]) 
             
           #'  Detection state
           for(j in 1:nsurveys){
             y[i,j] ~ dbern(p[i,j])
             p[i,j] <- 1 - pow((1 - r[i,j]), N[i])
-            logit(r[i,j]) <- alpha0 + alpha1[setup[i]]
+            logit(r[i,j]) <- alpha0 + a.setup[setup[i]]
           }
         }
           
         #'  Derived parameters
         #'  ------------------
-        #'  Mean lambda per year at road sites
+        #'  Mean lambda per year
         for(yr in 1:nyear) {
-          lambdaYr[yr] <- exp(beta0 + beta1[yr])
+          lambdaYr[yr] <- exp(beta0 + b.year[yr])
         }
   
-        #'  Mean lambda averaged across years
-        mu.lambda <- mean(lambdaYr[])
-          
-        #'  Total abundance across camera sites
-        totalN <- sum(N[])
-          
-        #'  Total sites occupied (N > 0)
-        for(i in 1:nsites) {
-          occupied[i] <- ifelse(N[i] > 0, 1, 0)
-        }
-        occSites <- sum(occupied[])
-    
-        #'  Mean occupancy probability
-        mean.psi <- 1 - exp(-mu.lambda)
-     
         #'  Mean per-individual detection probability (r) per camera setup
         for(cam in 1:nsets) {
-          rSetup[cam] <- 1/(1 + exp(-(alpha0 + alpha1[cam])))
+          rSetup[cam] <- 1/(1 + exp(-(alpha0 + a.setup[cam])))
         }
         #'  per-individual detection probability (r) averaged across all camera setups 
         mu.r <- mean(rSetup[])
