a255a811a21e5ec3651b8244ae727fbb587abeb0
Bassing <w76r112@msu.montana.edu>
2026-01-06 17:35:39 -0700

    Notes to self, function to stack time-lagged posteriors per spp

--- Scripts/Structural_Equation_Models/Format_RNmodel_Posteriors_for_SEM.R
+++ Scripts/Structural_Equation_Models/Format_RNmodel_Posteriors_for_SEM.R
@@ -44,18 +44,21 @@
       cluster_posteriors[i,] <- mod_out[[i]][[1]]
     }
   
-    #'  Turn matrix into a data frame with uniqueCluster as first column
-    uniqueCluster <- as.data.frame(seq(1:nrow(cluster_posteriors)))
-    names(uniqueCluster) <- "uniqueCluster"
-    cluster_posteriors <- cbind(uniqueCluster, cluster_posteriors) 
+    #' #'  Turn matrix into a data frame with uniqueCluster as first column
+    #' uniqueCluster <- as.data.frame(seq(1:nrow(cluster_posteriors)))
+    #' names(uniqueCluster) <- "uniqueCluster"
+    #' cluster_posteriors <- cbind(uniqueCluster, cluster_posteriors) 
+    
+    ####  THIS NEEDS TO BE UPDATED WITH BETTER CLUSTER INFO
+    ####  ADD IN COVARIATE DATA HERE TOO!!!!
     
     return(cluster_posteriors)
   }
   #'  Names of estimated posteriors of interest per year
   rdi.cl_2020 <- c("rdi.cl1", "rdi.cl2", "rdi.cl3", "rdi.cl4", "rdi.cl5", "rdi.cl6", "rdi.cl7", "rdi.cl8", "rdi.cl9")
   rdi.cl_2021.2022 <- c("rdi.cl1", "rdi.cl2", "rdi.cl3", "rdi.cl4", "rdi.cl5", "rdi.cl6", "rdi.cl7", "rdi.cl8", "rdi.cl9", "rdi.cl10", 
-                              "rdi.cl11", "rdi.cl12", "rdi.cl13", "rdi.cl14", "rdi.cl15", "rdi.cl16", "rdi.cl17", "rdi.cl18", "rdi.cl19", "rdi.cl20", 
-                              "rdi.cl21", "rdi.cl22", "rdi.cl23", "rdi.cl24")
+                        "rdi.cl11", "rdi.cl12", "rdi.cl13", "rdi.cl14", "rdi.cl15", "rdi.cl16", "rdi.cl17", "rdi.cl18", "rdi.cl19", "rdi.cl20", 
+                        "rdi.cl21", "rdi.cl22", "rdi.cl23", "rdi.cl24")
   #'  Run function for each year and species
   posteriors_20s <- lapply(mods_yr1, cluster_mcmc_matrix, cluster_out = rdi.cl_2020)
   posteriors_21s <- lapply(mods_yr2, cluster_mcmc_matrix, cluster_out = rdi.cl_2021.2022)
@@ -69,14 +72,39 @@
   
   #'  Double check these look right
   #'  First 10 iterations for each cluster in matrix vs model output
-  print(posteriors_20s[[1]][1:9, 1:10]) # wolf (2020)
+  print(posteriors_20s[[1]][, 1:10]) # wolf (2020)
   RN_wolf_20s$sims.list$rdi.cl1[1:10]; RN_wolf_20s$sims.list$rdi.cl9[1:10]
-  print(posteriors_21s[[3]][1:9, 1:10]) # bear (2021)
+  print(posteriors_21s[[3]][, 1:10]) # bear (2021)
   RN_bear_21s$sims.list$rdi.cl1[1:10]; RN_bear_21s$sims.list$rdi.cl9[1:10]
-  print(posteriors_22s[[7]][1:9, 1:10]) # white-tailed deer (2022)
+  print(posteriors_22s[[7]][, 1:10]) # white-tailed deer (2022)
   RN_wtd_22s$sims.list$rdi.cl1[1:10]; RN_wtd_22s$sims.list$rdi.cl9[1:10]
   
+  #'  Stack species-specific estimated posteriors across iterations 
+  #'  (e.g., time t = wolf 2021 and 2022 vs. time t-1 = wolf 2020 and 2021)
+  stacked_posteriors <- function(yr1, yr2, yr3) {
+    #'  Posteriors from time t 
+    time_t <- rbind(yr2, yr3)
+    #'  Posteriors from time t-1, given an annual lag from time t
+    time_tmin1 <- rbind(yr1, yr2)
+    
+    #'  List posteriors from time t and t-1
+    timelag_list <- list(time_t, time_tmin1)
+    return(timelag_list)
+  }
+  #'  Run function for each species
+  wolf_timelag <- stacked_posteriors(posteriors_20s[[1]], posteriors_21s[[1]], posteriors_22s[[1]])
+  lion_timelag <- stacked_posteriors(posteriors_20s[[2]], posteriors_21s[[2]], posteriors_22s[[2]])
+  bear_timelag <- stacked_posteriors(posteriors_20s[[3]], posteriors_21s[[3]], posteriors_22s[[3]])
+  coy_timelag <- stacked_posteriors(posteriors_20s[[4]], posteriors_21s[[4]], posteriors_22s[[4]])
+  elk_timelag <- stacked_posteriors(posteriors_20s[[5]], posteriors_21s[[5]], posteriors_22s[[5]])
+  moose_timelag <- stacked_posteriors(posteriors_20s[[6]], posteriors_21s[[6]], posteriors_22s[[6]])
+  wtd_timelag <- stacked_posteriors(posteriors_20s[[7]], posteriors_21s[[7]], posteriors_22s[[7]])
   
+  #'  Double check everything looks right
+  print(wolf_timelag[[1]][,1:10])
+  print(wolf_timelag[[2]][,1:10])
+  print(elk_timelag[[1]][,1:10])
+  print(elk_timelag[[2]][,1:10])
   
   
   
