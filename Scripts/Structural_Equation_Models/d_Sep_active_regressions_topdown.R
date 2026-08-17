34690c1630dd1d6b766daf1de2338a0636557ecb
SarahBassing <sarah.bassing@gmail.com>
2026-08-17 14:56:21 -0600

    add time-defying independence claims back in

--- Scripts/Structural_Equation_Models/d_Sep_active_regressions_topdown.R
+++ Scripts/Structural_Equation_Models/d_Sep_active_regressions_topdown.R
@@ -37,6 +37,7 @@
   #'  ---------------------------------------------------------------- 
   
   dSep_iterations_topdown <- list(
+    #'  Conditional independence claims
     # Regression #1
     # bs_topdown[[1]] # "deerHarv.tmin1" "coy.t"          "coy.tmin1"
     list(dSep_test = 4, covariates = c("coy.latent", "deerHarv"), spp = c(".coy", ".harvest"), indices = as.integer(c(1,1)), lags = c("y-1","y-1")),
@@ -124,30 +125,42 @@
     # Regression #29
     # bs_topdown[[29]] # "lion.tmin1"     "lion.t"         "lionHarv.tmin1"
     list(dSep_test = 1, covariates = c("lionHarv", "lion.latent"), spp = c(".harvest", ".lion"), indices = as.integer(c(1,1)), lags = c("y-1","y-1")),
-    # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
-    # # bs_topdown[[30]] # "wtd.t"          "elk.tmin1"      "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
-    # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
-    # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
-    # # bs_topdown[[31]] # "wtd.t"          "coy.tmin1"      "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
-    # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
-    # Regression #30
+    
+    ##########
+    # Regression #30   # NOTE: I'VE FLIPPED THE RESPONSE & EXPLANATORY VARIABLES HERE SO THE INDEP. CLAIM IS TEMPORALLY VALID
+    # bs_topdown[[30]] # "wtd.t"          "elk.tmin1"      "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
+    list(dSep_test = 7, covariates = c("deerHarv.latent", "wtd.latent", "lion.latent", "elk.latent"), spp = c(".harvest", ".wtd", ".lion", ".elk"), indices = as.integer(c(1,1,1,1)), lags = c("y-1","y-1","y-1","y-1")),
+    # Regression #31   #  NOTE: I'VE FLIPPED THE RESPONSE & EXPLANATORY VARIABLES HERE SO THE INDEP. CLAIM IS TEMPORALLY VALID
+    # bs_topdown[[31]] # "wtd.t"          "coy.tmin1"      "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
+    list(dSep_test = 7, covariates = c("deerHarv.latent", "wtd.latent", "lion.latent", "coy.latent"), spp = c(".harvest", ".wtd", ".lion", ".coy"), indices = as.integer(c(1,1,1,1)), lags = c("y-1","y-1","y-1","y-1")),
+    ##########
+    
+    # Regression #32
     # bs_topdown[[32]] # "wtd.t"          "coy.t"          "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"     "coy.tmin1"
     list(dSep_test = 4, covariates = c("deerHarv", "wtd.latent", "lion.latent", "coy.latent", "wtd.latent"), spp = c(".harvest", ".wtd", ".lion", ".coy", ".wtd"), indices = as.integer(c(1,1,1,1,2)), lags = c("y-1","y-1","y-1","y-1","y")),
-    # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
-    # # bs_topdown[[33]] # "wtd.t"          "bearHarv.tmin1" "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
-    # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
-    # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
-    # # bs_topdown[[34]] # "wtd.t"          "bear.tmin1"     "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
-    # list(dSep_test = 7, covariates = c(), spp = c(), indices = as.integer(c())),
+    
+    ##########
+    # Regression #33   # NOTE: I'VE FLIPPED THE RESPONSE & EXPLANATORY VARIABLES HERE SO THE INDEP. CLAIM IS TEMPORALLY VALID
+    # bs_topdown[[33]] # "wtd.t"          "bearHarv.tmin1" "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
+    list(dSep_test = 7, covariates = c("deerHarv.latent", "wtd.latent", "lion.latent", "bearHarv.latent"), spp = c(".harvest", ".wtd", ".lion", ".harvest"), indices = as.integer(c(1,1,1,2)), lags = c("y-1","y-1","y-1","y-1")),
+    # Regression #NA   # NOTE: I'VE FLIPPED THE RESPONSE & EXPLANATORY VARIABLES HERE SO THE INDEP. CLAIM IS TEMPORALLY VALID
+    # bs_topdown[[34]] # "wtd.t"          "bear.tmin1"     "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
+    list(dSep_test = 7, covariates = c("deerHarv.latent", "wtd.latent", "lion.latent", "bear.latent"), spp = c(".harvest", ".wtd", ".lion", ".bear"), indices = as.integer(c(1,1,1,1)), lags = c("y-1","y-1","y-1","y-1")),
+    ##########
+    
     # Regression #31
     # bs_topdown[[35]] # "wtd.t"          "bear.t"         "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"     "bearHarv.tmin1" "bear.tmin1"
     list(dSep_test = 3, covariates = c("deerHarv", "wtd.latent", "lion.latent", "bearHarv", "bear.latent", "wtd.latent"), spp = c(".harvest", ".wtd", ".lion", ".harvest", ".bear", ".wtd"), indices = as.integer(c(1,1,1,2,1,2)), lags = c("y-1","y-1","y-1","y-1","y-1","y")),
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[36]] # "wtd.t"          "wolfHarv.tmin1" "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
     # # Regression #NA   NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[37]] # "wtd.t"          "wolf.tmin1"     "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #32
     # bs_topdown[[38]] # "wtd.t"          "moose.t"        "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"     "moose.tmin1"    "wolf.tmin1"
     list(dSep_test = 6, covariates = c("deerHarv", "wtd.latent", "lion.latent", "moose.latent", "wolf.latent", "wtd.latent"), spp = c(".harvest", ".wtd", ".lion", ".moose", ".wolf", ".wtd"), indices = as.integer(c(1,1,1,1,1,2)), lags = c("y-1","y-1","y-1","y-1","y-1","y")),
@@ -157,9 +170,14 @@
     # Regression #34
     # bs_topdown[[40]] # "wtd.t"          "wolf.t"         "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"     "wolfHarv.tmin1" "wolf.tmin1"
     list(dSep_test = 2, covariates = c("deerHarv", "wtd.latent", "lion.latent", "wolfHarv", "wolf.latent", "wtd.latent"), spp = c(".harvest", ".wtd", ".lion", ".harvest", ".wolf", ".wtd"), indices = as.integer(c(1,1,1,2,1,2)), lags = c("y-1","y-1","y-1","y-1","y-1","y")),
+    
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[41]] #  "wtd.t"          "lionHarv.tmin1" "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"
     # list(dSep_test = , covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #35
     # bs_topdown[[42]] # "wtd.t"          "lion.t"         "deerHarv.tmin1" "wtd.tmin1"      "lion.tmin1"     "lionHarv.tmin1"
     list(dSep_test = 1, covariates = c("deerHarv", "wtd.latent", "lion.latent", "lionHarv", "wtd.latent"), spp = c(".harvest", ".wtd", ".lion", ".harvest", ".wtd"), indices = as.integer(c(1,1,1,2,2)), lags = c("y-1","y-1","y-1","y-1","y")),
@@ -193,21 +211,29 @@
     # Regression #45
     # bs_topdown[[52]] # "coy.tmin1"      "lion.t"         "lionHarv.tmin1"
     list(dSep_test = 1, covariates = c("lionHarv", "coy.latent"), spp = c(".harvest", ".coy"), indices = as.integer(c(1,1)), lags = c("y-1","y-1")),
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[53]] # "coy.t"          "bearHarv.tmin1" "coy.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[54]] # "coy.t"      "bear.tmin1" "coy.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #46
     # bs_topdown[[55]] # "coy.t"          "bear.t"         "coy.tmin1"      "bearHarv.tmin1" "bear.tmin1"
     list(dSep_test = 3, covariates = c("coy.latent", "bearHarv", "bear.latent", "coy.latent"), spp = c(".coy", ".harvest", ".bear", ".coy"), indices = as.integer(c(1,1,1,2)), lags = c("y-1","y-1","y-1","y")),
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[56]] # "coy.t"          "wolfHarv.tmin1" "coy.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[57]] # "coy.t"      "wolf.tmin1" "coy.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #47
     # bs_topdown[[58]] # "coy.t"       "moose.t"     "coy.tmin1"   "moose.tmin1" "wolf.tmin1"
     list(dSep_test = 6, covariates = c("coy.latent", "moose.latent", "wolf.latent", "coy.latent"), spp = c(".coy", ".moose", ".wolf", ".coy"), indices = as.integer(c(1,1,1,2)), lags = c("y-1","y-1","y-1","y")),
@@ -217,9 +243,13 @@
     # Regression #49
     # bs_topdown[[60]] # "coy.t"          "wolf.t"         "coy.tmin1"      "wolfHarv.tmin1" "wolf.tmin1"
     list(dSep_test = 2, covariates = c("coy.latent", "wolfHarv", "wolf.latent", "coy.latent"), spp = c(".coy", ".harvest", ".wolf", ".coy"), indices = as.integer(c(1,1,1,2)), lags = c("y-1","y-1","y-1","y")),
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[61]] # "coy.t"          "lionHarv.tmin1" "coy.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #50
     # bs_topdown[[62]] # "coy.t"          "lion.t"         "coy.tmin1"      "lionHarv.tmin1"
     list(dSep_test = 1, covariates = c("coy.latent", "lionHarv", "coy.latent"), spp = c(".coy", ".harvest", ".coy"), indices = as.integer(c(1,1,2)), lags = c("y-1","y-1","y")),
@@ -247,12 +277,16 @@
     # Regression #58
     # bs_topdown[[70]] # "bear.tmin1"     "lion.t"         "lionHarv.tmin1"
     list(dSep_test = 1, covariates = c("lionHarv", "bear.latent"), spp = c(".harvest", ".bear"), indices = as.integer(c(1,1)), lags = c("y-1","y-1")),
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[71]] # "bear.t"         "wolfHarv.tmin1" "bearHarv.tmin1" "bear.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[72]] # "bear.t"         "wolf.tmin1"     "bearHarv.tmin1" "bear.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #59
     # bs_topdown[[73]] # "bear.t"         "moose.t"        "bearHarv.tmin1" "bear.tmin1"     "moose.tmin1"    "wolf.tmin1"
     list(dSep_test = 6, covariates = c("bearHarv", "bear.latent", "moose.latent", "wolf.latent", "bear.latent"), spp = c(".harvest", ".bear", ".moose", ".wolf", ".bear"), indices = as.integer(c(1,1,1,1,2)), lags = c("y-1","y-1","y-1","y-1","y")),
@@ -262,9 +296,13 @@
     # Regression #61
     # bs_topdown[[75]] # "bear.t"         "wolf.t"         "bearHarv.tmin1" "bear.tmin1"     "wolfHarv.tmin1" "wolf.tmin1"
     list(dSep_test = 2, covariates = c("bearHarv", "bear.latent", "wolfHarv", "wolf.latent", "bear.latent"), spp = c(".harvest", ".bear", ".harvest", ".wolf", ".bear"), indices = as.integer(c(1,1,2,1,2)), lags = c("y-1","y-1","y-1","y-1","y")),
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[76]] # "bear.t"         "lionHarv.tmin1" "bearHarv.tmin1" "bear.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #62
     # bs_topdown[[77]] # "bear.t"         "lion.t"         "bearHarv.tmin1" "bear.tmin1"     "lionHarv.tmin1"
     list(dSep_test = 1, covariates = c("bearHarv", "bear.latent", "lionHarv", "bear.latent"), spp = c(".harvest", ".bear", ".harvest", ".bear"), indices = as.integer(c(1,1,1,2)), lags = c("y-1","y-1","y-1","y")),
@@ -286,22 +324,36 @@
     # Regression #68
     # bs_topdown[[83]] # "moose.t"        "wolf.t"         "moose.tmin1"    "wolf.tmin1"     "wolfHarv.tmin1"
     list(dSep_test = 2, covariates = c("moose.latent", "wolf.latent", "wolfHarv", "moose.latent"), spp = c(".moose", ".wolf", ".harvest", ".moose"), indices = as.integer(c(1,1,1,2)), lags = c("y-1","y-1","y-1","y")),
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[84]] # "moose.t"        "lionHarv.tmin1" "moose.tmin1"    "wolf.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #69
     # bs_topdown[[85]] # "moose.t"        "lion.t"         "moose.tmin1"    "wolf.tmin1"     "lionHarv.tmin1"
     list(dSep_test = 1, covariates = c("moose.latent", "wolf.latent", "lionHarv", "moose.latent"), spp = c(".moose", ".wtd", ".harvest", ".moose"), indices = as.integer(c(1,1,1,2)), lags = c("y-1","y-1","y-1","y")),
     # Regression #70
     # bs_topdown[[86]] # "elk.t"          "wolf.t"         "elkHarv.tmin1"  "lion.tmin1"     "elk.tmin1"      "wolf.tmin1"     "wolfHarv.tmin1"
     list(dSep_test = 2, covariates = c("elkHarv", "lion.latent", "elk.latent", "wolf.latent", "wolfHarv", "elk.latent"), spp = c(".harvest", ".lion", ".elk", ".wolf", ".harvest", ".elk"), indices = as.integer(c(1,1,1,1,2,2)), lags = c("y-1","y-1","y-1","y-1","y-1","y")),
+    
+    ##########
     # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[87]] # "elk.t"          "lionHarv.tmin1" "elkHarv.tmin1"  "lion.tmin1"     "elk.tmin1"      "wolf.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c())),
+    ##########
+    
     # Regression #71
     # bs_topdown[[88]] # "elk.t"          "lion.t"         "elkHarv.tmin1"  "lion.tmin1"     "elk.tmin1"      "wolf.tmin1"     "lionHarv.tmin1"
     list(dSep_test = 1, covariates = c("elkHarv", "lion.latent", "elk.latent", "wolf.latent", "lionHarv", "elk.latent"), spp = c(".harvest", ".lion", ".elk", ".wolf", ".harvest", ".elk"), indices = as.integer(c(1,1,1,1,2,2)), lags = c("y-1","y-1","y-1","y-1","y-1","y"))
+    
+    ##########
     # # # Regression #NA   #  NOT POSSIBLE - t cannot affect t-1
     # # bs_topdown[[89]] # "wolf.t"         "lionHarv.tmin1" "wolfHarv.tmin1" "wolf.tmin1"
     # list(dSep_test = NA, covariates = c(), spp = c(), indices = as.integer(c()))
+    ##########
+    
+    #'  Unconditional independence claims
+    
   )
