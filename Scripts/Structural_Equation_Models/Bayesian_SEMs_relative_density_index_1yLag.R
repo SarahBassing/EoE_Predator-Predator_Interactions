8d1ac948096f29c09cf922ea8c3256f3f4011ed4
SarahBassing <sarah.bassing@gmail.com>
2026-09-13 21:13:10 -0600

    clean up script, LOSO CV results table

--- Scripts/Structural_Equation_Models/Bayesian_SEMs_relative_density_index_1yLag.R
+++ Scripts/Structural_Equation_Models/Bayesian_SEMs_relative_density_index_1yLag.R
@@ -27,157 +27,7 @@
   
   #'  Set options so all no rows are omitted in model output
   options(max.print = 9999)
-  
-  
-  
-  #' #'  ------------------------
-  #' ####  Setup data for JAGS  ####
-  #' #'  ------------------------
-  #' #'  Bundle data for JAGS
-  #' bundle_dat <- function(dat, covs, nwolf, nlion, nbear, ncoy, nelk, nmoose, nwtd, nharv, nfor, nwsi) {
-  #'   #'  Bundle data for JAGS
-  #'   bundled <- list(nWolf = nwolf,
-  #'                   nLion = nlion, 
-  #'                   nBear = nbear, 
-  #'                   nCoy = ncoy, 
-  #'                   nElk = nelk,
-  #'                   nMoose = nmoose, 
-  #'                   nWtd = nwtd, 
-  #'                   nharvest = nharv,
-  #'                   nforest = nfor,
-  #'                   nWSI = nwsi,
-  #'                   nCluster = as.numeric(length(unique(dat$wolf$time_t$cluster))),
-  #'                   nSpp = 7,
-  #'                   #'  Standardized posterior means and SD for each species and time step
-  #'                   wolf.t_hat = dat$wolf$time_t$posterior_mu_z,
-  #'                   wolf.t.sigma_hat = dat$wolf$time_t$posterior_sd_z,
-  #'                   wolf.tmin1_hat = dat$wolf$time_tmin1$posterior_mu_z,
-  #'                   wolf.tmin1.sigma_hat = dat$wolf$time_tmin1$posterior_sd_z,
-  #'                   lion.t_hat = dat$lion$time_t$posterior_mu_z,
-  #'                   lion.t.sigma_hat = dat$lion$time_t$posterior_sd_z,
-  #'                   lion.tmin1_hat = dat$lion$time_tmin1$posterior_mu_z,
-  #'                   lion.tmin1.sigma_hat = dat$lion$time_tmin1$posterior_sd_z,
-  #'                   bear.t_hat = dat$bear$time_t$posterior_mu_z,
-  #'                   bear.t.sigma_hat = dat$bear$time_t$posterior_sd_z,
-  #'                   bear.tmin1_hat = dat$bear$time_tmin1$posterior_mu_z,
-  #'                   bear.tmin1.sigma_hat = dat$bear$time_tmin1$posterior_sd_z,
-  #'                   coy.t_hat = dat$coy$time_t$posterior_mu_z,
-  #'                   coy.t.sigma_hat = dat$coy$time_t$posterior_sd_z, 
-  #'                   coy.tmin1_hat = dat$coy$time_tmin1$posterior_mu_z,
-  #'                   coy.tmin1.sigma_hat = dat$coy$time_tmin1$posterior_sd_z,
-  #'                   elk.t_hat = dat$elk$time_t$posterior_mu_z,
-  #'                   elk.t.sigma_hat = dat$elk$time_t$posterior_sd_z,
-  #'                   elk.tmin1_hat = dat$elk$time_tmin1$posterior_mu_z,
-  #'                   elk.tmin1.sigma_hat = dat$elk$time_tmin1$posterior_sd_z,
-  #'                   moose.t_hat = dat$moose$time_t$posterior_mu_z,
-  #'                   moose.t.sigma_hat = dat$moose$time_t$posterior_sd_z,
-  #'                   moose.tmin1_hat = dat$moose$time_tmin1$posterior_mu_z,
-  #'                   moose.tmin1.sigma_hat = dat$moose$time_tmin1$posterior_sd_z,
-  #'                   wtd.t_hat = dat$wtd$time_t$posterior_mu_z,
-  #'                   wtd.t.sigma_hat = dat$wtd$time_t$posterior_sd_z,
-  #'                   wtd.tmin1_hat = dat$wtd$time_tmin1$posterior_mu_z,
-  #'                   wtd.tmin1.sigma_hat = dat$wtd$time_tmin1$posterior_sd_z,
-  #'                   #'  Standardized harvest and habitat variables for 1 year time lag
-  #'                   wolfHarv.tmin1 = covs$wolf_harv$time_tmin1$wolf_harv_z,
-  #'                   lionHarv.tmin1 = covs$lion_harv$time_tmin1$lion_harv_z,
-  #'                   bearHarv.tmin1 = covs$bear_harv$time_tmin1$bear_harv_z,
-  #'                   elkHarv.tmin1 = covs$elk_harv$time_tmin1$elk_harv_z,
-  #'                   mooseHarv.tmin1 = covs$moose_harv$time_tmin1$moose_harv_z,
-  #'                   deerHarv.tmin1 = covs$deer_harv$time_tmin1$deer_harv_z,
-  #'                   wsi.tmin1 = covs$wsi$time_tmin1$wsi_z,
-  #'                   forest.tmin1 = covs$prop_disturbed$time_tmin1$prop_disturb_z,
-  #'                   road.tmin1 = covs$road_density$time_tmin1$road_density_z,
-  #'                   public.tmin1 = covs$public_land$time_tmin1$public_land_z)
-  #'   str(bundled)
-  #'   return(bundled)
-  #' }
-  #' data_JAGS_bundle_ar1 <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 1, nlion = 1, nbear = 1, 
-  #'                                    ncoy = 1, nelk = 1, nmoose = 1, nwtd = 1, nharv = 0, nfor = 0, nwsi = 0)
-  #' data_JAGS_bundle_top <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 4, nlion = 3, nbear = 3, 
-  #'                                         ncoy = 2, nelk = 1, nmoose = 1, nwtd = 1, nharv = 6, nfor = 0, nwsi = 0)
-  #' data_JAGS_bundle_bottom <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 1, nlion = 1, nbear = 1, 
-  #'                                       ncoy = 1, nelk = 5, nmoose = 2, nwtd = 3, nharv = 0, nfor = 4, nwsi = 3)
-  #' data_JAGS_bundle_topinter <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 7, nlion = 4, nbear = 5, 
-  #'                                         ncoy = 2, nelk = 1, nmoose = 1, nwtd = 1, nharv = 6, nfor = 1, nwsi = 1)
-  #' data_JAGS_bundle_topinter_reduced <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 6, nlion = 4, nbear = 2, 
-  #'                                         ncoy = 1, nelk = 1, nmoose = 1, nwtd = 1, nharv = 3, nfor = 1, nwsi = 1)
-  #' # data_JAGS_bundle_topexploit <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 4, nlion = 3, nbear = 1, 
-  #' #                                           ncoy = 1, nelk = 1, nmoose = 1, nwtd = 1, nharv = 3, nfor = 1, nwsi = 1)
-  #' data_JAGS_bundle_bottominter <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 4, nlion = 1, nbear = 1, 
-  #'                                            ncoy = 1, nelk = 5, nmoose = 2, nwtd = 3, nharv = 0, nfor = 4, nwsi = 3)
-  #' data_JAGS_bundle_bottominter_reduced <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 4, nlion = 1, nbear = 1, 
-  #'                                                    ncoy = 1, nelk = 2, nmoose = 2, nwtd = 3, nharv = 0, nfor = 4, nwsi = 3)
-  #' data_JAGS_bundle_topbottom <- bundle_dat(post_summaries, covs = covs_ztransformed, nwolf = 7, nlion = 4, nbear = 2, 
-  #'                                            ncoy = 1, nelk = 3, nmoose = 2, nwtd = 2, nharv = 5, nfor = 4, nwsi = 3)
-  #' 
-  #'                                
-  #' # save(data_JAGS_bundle_topinter, file = "./Outputs/SEM/JAGS_data_bundle/data_JAGS_bundle_topinter.RData")
-  #' # save(data_JAGS_bundle_topexploit, file = "./Outputs/SEM/JAGS_data_bundle/data_JAGS_bundle_topexploit.RData")
-  #' # save(data_JAGS_bundle_bottominter, file = "./Outputs/SEM/JAGS_data_bundle/data_JAGS_bundle_bottominter.RData")
-  #' # save(data_JAGS_bundle_bottomexploit, file = "./Outputs/SEM/JAGS_data_bundle/data_JAGS_bundle_bottomexploit.RData")
-  #' 
-  #' #'  Generate initial values for each parameter (random node)
-  #' generate_inits <- function(nwolf, nlion, nbear, ncoy, nelk, nmoose, nwtd, nharv, nfor, nwsi) {
-  #'   
-  #'   #'  Generate random values for each species-specific beta (nwolf, nlion, etc.
-  #'   #'  based on number of species-specific betas to be estimated)
-  #'   list(
-  #'     wolf.t_1 = runif(nwolf, -0.5, 0.5),  # consider -1, 1
-  #'     lion.t_1 = runif(nlion, -0.5, 0.5),
-  #'     bear.t_1 = runif(nbear, -0.5, 0.5),
-  #'     coy.t_1 = runif(ncoy, -0.5, 0.5),
-  #'     elk.t_1 = runif(nelk, -0.5, 0.5),
-  #'     moose.t_1 = runif(nmoose, -0.5, 0.5),
-  #'     wtd.t_1 = runif(nwtd, -0.5, 0.5),
-  #'     wolfharvest.t_1 = runif(nharv, -0.5, 0.5),
-  #'     lionharvest.t_1 = runif(nharv, -0.5, 0.5),
-  #'     bearharvest.t_1 = runif(nharv, -0.5, 0.5),
-  #'     elkharvest.t_1 = runif(nharv, -0.5, 0.5),
-  #'     mooseharvest.t_1 = runif(nharv, -0.5, 0.5),
-  #'     deerharvest.t_1 = runif(nharv, -0.5, 0.5),
-  #'     wsi.t_1 = runif(nwsi, -0.5, 0.5),
-  #'     forest.t_1 = runif(nfor, -0.5, 0.5),
-  #'     road.t_1 = runif(nwsi, -0.5, 0.5),
-  #'     public.t_1 = runif(nwsi, -0.5, 0.5) # doesn't matter that using nwsi or other b/c all = 1
-  #'     #' #'  Fix random number generator and seed for every run of this function
-  #'     #' .RNG.name = "base::Wichmann-Hill",
-  #'     #' .RNG.seed = 182  
-  #'     #' #'  Setting RNG seed leads to the same random number stream during
-  #'     #' #'  adaptation and sampling, causing different inits to be rapidly erased
-  #'     #' #'  and all chains to follow the same deterministic path. This does not 
-  #'     #' #'  appear to happen with the cross lag model though.
-  #'   )
-  #' }
-  #' #'  Define number of chains
-  #' num.chains <- 3
-  #' #'  Create empty lists
-  #' initsList_ar1 <- initsList_topdown <- initsList_bottomup <- vector('list', num.chains) 
-  #' initsList_topinter <- initsList_topexploit <- initsList_bottominter <- initsList_topbottom <- vector('list', num.chains)
-  #' initsList_topinter_reduced <- initsList_bottominter_reduced <- vector('list', num.chains)
-  #' #'  Setting seed for reproducibility
-  #' set.seed(9983)
-  #' #'  Loop through generate_inits function 3 times (1 for each chain) 
-  #' for(i in 1:num.chains){
-  #'   initsList_ar1[[i]] <- generate_inits(nwolf = 1, nlion = 1, nbear = 1, ncoy = 1, nelk = 1, 
-  #'                                    nmoose = 1, nwtd = 1, nharv = 0, nfor = 0, nwsi = 0)
-  #'   initsList_topdown[[i]] <- generate_inits(nwolf = 4, nlion = 3, nbear = 3, ncoy = 2, nelk = 1, 
-  #'                                            nmoose = 1, nwtd = 1, nharv = 6, nfor = 0, nwsi = 0)
-  #'   initsList_bottomup[[i]] <- generate_inits(nwolf = 1, nlion = 1, nbear = 1, ncoy = 1, nelk = 5, 
-  #'                                             nmoose = 2, nwtd = 3, nharv = 0, nfor = 4, nwsi = 3)
-  #'   initsList_topinter[[i]] <- generate_inits(nwolf = 7, nlion = 4, nbear = 5, ncoy = 2, nelk = 1, 
-  #'                                             nmoose = 1, nwtd = 1, nharv = 6, nfor = 0, nwsi = 0)
-  #'   initsList_topinter_reduced[[i]] <- generate_inits(nwolf = 6, nlion = 4, nbear = 2, ncoy = 1, nelk = 1, 
-  #'                                             nmoose = 1, nwtd = 1, nharv = 1, nfor = 0, nwsi = 0)
-  #'   # initsList_topexploit[[i]] <- generate_inits(nwolf = 4, nlion = 3, nbear = 1, ncoy = 1, nelk = 1, 
-  #'   #                                           nmoose = 1, nwtd = 1, nharv = 3, nfor = 0, nwsi = 0)
-  #'   initsList_bottominter[[i]] <- generate_inits(nwolf = 4, nlion = 1, nbear = 1, ncoy = 1, nelk = 5, 
-  #'                                                nmoose = 2, nwtd = 3, nharv = 0, nfor = 4, nwsi = 3)
-  #'   initsList_bottominter_reduced[[i]] <- generate_inits(nwolf = 4, nlion = 1, nbear = 1, ncoy = 1, nelk = 2, 
-  #'                                                        nmoose = 2, nwtd = 3, nharv = 0, nfor = 4, nwsi = 3)
-  #'   initsList_topbottom[[i]] <- generate_inits(nwolf = 7, nlion = 4, nbear = 2, ncoy = 1, nelk = 3, 
-  #'                                              nmoose = 2, nwtd = 2, nharv = 5, nfor = 4, nwsi = 3)
-  #' }
-  
+ 
   #'  Parameters monitored
   params <- c("beta.int", "beta.int.tmin1", "beta.wolf", "beta.lion", "beta.bear", 
               "beta.coy", "beta.elk", "beta.moose", "beta.wtd", "beta.harvest", 
@@ -633,7 +483,7 @@
   loso_bottomup_exploit <- run_loso_cv(mod = "./Outputs/SEM/JAGS_out/JAGS_SEM_bottomup_final.txt", dat = data_JAGS_bundle_bottomup_final,
                                       spp_names = c("lion", "wolf", "bear", "coy", "elk", "moose", "wtd"), nSites = 23,  
                                       nYear = 4, n.chains = nc, n.adapt = na, n.burnin = nb, n.iter = ni, n.thin = nt,
-                                      out_dir = )
+                                      out_dir = "./Outputs/SEM/LOSO_CV/bottomup_exploit")
   end.time <- Sys.time(); (run.time <- end.time - start.time)
   save(loso_bottomup_exploit, file = "./Outputs/SEM/LOSO_CV/LOSO_CV_bottomup_exploit.RData")
   
@@ -645,11 +495,11 @@
   end.time <- Sys.time(); (run.time <- end.time - start.time)
   save(loso_bottomup_inter, file = "./Outputs/SEM/LOSO_CV/LOSO_CV_bottomup_inter.RData")
   
-  #'  Read in LOSO_CV results
-  load("./Outputs/SEM/LOSO_CV/LOSO_CV_topdown_exploit.RData")
-  load("./Outputs/SEM/LOSO_CV/LOSO_CV_topdown_inter.RData")
-  load("./Outputs/SEM/LOSO_CV/LOSO_CV_bottomup_exploit.RData")
-  load("./Outputs/SEM/LOSO_CV/LOSO_CV_bottomup_inter.RData")
+  #' #'  Read in LOSO_CV results
+  #' load("./Outputs/SEM/LOSO_CV/LOSO_CV_topdown_exploit.RData")
+  #' load("./Outputs/SEM/LOSO_CV/LOSO_CV_topdown_inter.RData")
+  #' load("./Outputs/SEM/LOSO_CV/LOSO_CV_bottomup_exploit.RData")
+  #' load("./Outputs/SEM/LOSO_CV/LOSO_CV_bottomup_inter.RData")
   
   #'  Compare LOSO CV across models
   compare_loso <- data.frame(model = c("topdown_exploitative", "topdown_interference", 
@@ -666,6 +516,18 @@
   #' Differences in elpd are worth treating as meaningful when |elpd_diff| is roughly >=2x its se
   #' In other words, the models are distinguishable when |elpd_diff| is meaningfully larger than its se
   
+  #'  LOSO CV results table
+  loso_tbl <- compare_loso %>%
+    transmute(Model = model, ELPD = elpd, SE = se, elpd_diff = elpd_diff) %>%
+    mutate(Model = ifelse(Model == "topdown_exploitative", "Top-down, exploitative", Model),
+           Model = ifelse(Model == "topdown_interference", "Top-down, interference", Model),
+           Model = ifelse(Model == "bottomup_exploitative", "Bottom-up, exploitative", Model),
+           Model = ifelse(Model == "bottomup_interference", "Bottom-up, interference", Model)) %>%
+    arrange(-elpd_diff)
+  names(loso_tbl)[names(loso_tbl) == "elpd_diff"] <- paste0("\u0394", "ELPD")  #' "ΔELPD"
+  print(loso_tbl)
+  write.csv(loso_tbl, "./Outputs/SEM/Tables_for_publication/LOSO_CV_results.csv")
+  
   #'  Paired comparisons to evaluate site-by-site differences between models 
   #'  Some sites were likely harder to estimate than others and this problem likely
   #'  arose for each model so comparing paired models helps differentiate them
@@ -676,9 +538,20 @@
     se_diff <- sd(diff_vec) * sqrt(length(diff_vec))
     list(elpd_diff = elpd_diff, se_diff = se_diff, ratio = abs(elpd_diff) / se_diff) # >= ~2 suggests meaningful difference
   }
-  compare_paired_mods(loso_bottomup_exploit$elpd_per_site, loso_topdown_exploit$elpd_per_site)
-  compare_paired_mods(loso_bottomup_exploit$elpd_per_site, loso_topdown_inter$elpd_per_site)   # suggests a meaningful difference from bottomup_exploit
-  compare_paired_mods(loso_bottomup_exploit$elpd_per_site, loso_bottomup_inter$elpd_per_site)
+  (compare_topdown <- compare_paired_mods(loso_bottomup_exploit$elpd_per_site, loso_topdown_exploit$elpd_per_site))
+  (compare_topdown_inter <- compare_paired_mods(loso_bottomup_exploit$elpd_per_site, loso_topdown_inter$elpd_per_site))   # suggests a meaningful difference from bottomup_exploit
+  (compare_bottomup_inter <- compare_paired_mods(loso_bottomup_exploit$elpd_per_site, loso_bottomup_inter$elpd_per_site))
+  
+  loso_pair <- c("Top-down, exploitative", "Top-down, interference", "Bottom-up, interference")
+  loso_compare_tbl <- data.frame(Model = loso_pair, 
+                                 elpd_diff = c(compare_topdown$elpd_diff, compare_topdown_inter$elpd_diff, compare_bottomup_inter$elpd_diff),
+                                 se_diff = c(compare_topdown$se_diff, compare_topdown_inter$se_diff, compare_bottomup_inter$se_diff),
+                                 Ratio = c(compare_topdown$ratio, compare_topdown_inter$ratio, compare_bottomup_inter$ratio)) %>%
+    arrange(Ratio)
+  names(loso_compare_tbl)[names(loso_compare_tbl) == "elpd_diff"] <- paste0("\u0394", "ELPD")  #' "ΔELPD"
+  names(loso_compare_tbl)[names(loso_compare_tbl) == "se_diff"] <- paste0("\u0394", "SE")  #' "ΔELPD"
+  print(loso_compare_tbl)
+  write.csv(loso_compare_tbl, "./Outputs/SEM/Tables_for_publication/LOSO_CV_compared2BottomUp_results.csv")
   
   #'  bottomup_exploit and topdown_exploit are essentially indistinguishable 
   #'  bottomup_inter is not meaningfully different from bottomup_exploit
